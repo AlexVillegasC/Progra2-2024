68363e7226fc548202c32039b845fbf4356885a1
heyitskarinamd@outlook.com <heyitskarinamd@outlook.com>
2024-03-09 11:27:45 -0600

    Prueba 2

---
+++ Labs/KarinaMoreno-504520784/Pokemon.cs
@@ -0,0 +1,18 @@
+﻿namespace Lab1.KarinaMoreno_504520784;
+
+public class Pokemon
+{
+    private int Vida { get; set; }
+
+    private int Ataque { get; set; }
+    
+    private string Nombre { get; set; }
+
+    public Pokemon (int ataque, int vida, string nombre) 
+    {
+        this.Ataque = ataque;
+        this.Vida = vida;
+        this.Nombre = nombre;
+    }
+
+}
