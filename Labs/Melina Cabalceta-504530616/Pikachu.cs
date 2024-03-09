8ae0910e357507b436339c126168da3507a14b38
Melina1304 <melina.cabalceta.hernandez@est.una.ac.cr>
2024-03-09 11:56:37 -0600

    Lab1 batalla pokemon

---
+++ Labs/Melina Cabalceta-504530616/Pikachu.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Lab1.Melina_Cabalceta_504530616
+{
+    public class Pikachu : Pokemon //Pikachu hereda de Pokemon 
+    {
+        public Pikachu (int Ataque)
+        {
+            this.Ataque = Ataque;
+            this.nombre = "Pikachu";
+        }
+    }
+}
