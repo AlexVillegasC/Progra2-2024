de47c68bb0a9b9dc9f0ddb787b8f9664c54fca0a
Brandon-Corrales <brandon.nunez.corrales@est.una.ac.cr>
2024-03-09 11:53:29 -0600

    [Lab 1] Pokemon -Clases

---
+++ Labs/Brandon-Nuñez-305370647/Gengar.cs
@@ -0,0 +1,17 @@
+﻿
+using global::Lab1.AlexVillegas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab1.Brandon_Nunez;
+
+public class Gengar : Pokemon
+{
+    public Gengar(int ataque, int vida, string nombre) : base(ataque, vida, nombre)
+    {
+        this.Ataque = ataque;
+        this.Vida = vida;
+        this.Nombre = nombre;
+    }
+}
---
+++ Labs/Brandon-Nuñez-305370647/Pikachu.cs
@@ -0,0 +1,18 @@
+﻿
+using Lab1.AlexVillegas;
+
+namespace Lab1.Brandon_Nunez;
+
+public class Pikachu : Pokemon
+{
+    /// <summary>
+    /// Mi clase Pikachu
+    /// </summary>
+    /// <param name="ataque">Representa un ataque</param>
+    /// <param name="vida"></param>
+    /// <param name="nombre"></param>
+    public Pikachu(int ataque, int vida, string nombre) : base(ataque, vida, nombre)
+    {
+    }
+}
+
