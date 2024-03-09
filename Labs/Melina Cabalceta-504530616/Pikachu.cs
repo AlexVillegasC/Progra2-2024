f7031e9acda34674f99a86b1305884464d2e1937
Melina1304 <melina.cabalceta.hernandez@est.una.ac.cr>
2024-03-09 11:56:37 -0600

    Lab1 batalla pokemon

--- Labs/Melina Cabalceta-504530616/Pikachu.cs
+++ Labs/Melina Cabalceta-504530616/Pikachu.cs
@@ -2,12 +2,21 @@
 
 namespace Lab1.Melina_Cabalceta_504530616
 {
+<<<<<<< HEAD
     public class Pikachu : Pokemon  
     {
         
 
         public Pikachu(int ataque, int vida, string nombre) : base(ataque, vida, nombre)
         {
+=======
+    public class Pikachu : Pokemon //Pikachu hereda de Pokemon 
+    {
+        public Pikachu (int Ataque)
+        {
+            this.Ataque = Ataque;
+            this.nombre = "Pikachu";
+>>>>>>> 8ae0910 (Lab1 batalla pokemon)
         }
     }
 }
--- Labs/Melina Cabalceta-504530616/Pokemon.cs
+++ Labs/Melina Cabalceta-504530616/Pokemon.cs
@@ -1,14 +1,18 @@
 ﻿
+<<<<<<< HEAD
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+=======
+>>>>>>> 8ae0910 (Lab1 batalla pokemon)
 
 namespace Lab1.Melina_Cabalceta_504530616
 {
     public class Pokemon
     {
+<<<<<<< HEAD
        public int Ataque { get; set; } 
 
         public int Vida { get; set; }
@@ -20,6 +24,17 @@ namespace Lab1.Melina_Cabalceta_504530616
             Ataque = ataque;
             Vida = vida;
             Nombre = nombre;
+=======
+       public int Vida { get; set; } = 100;
+
+        public int Ataque { get; set; }
+
+        public string Nombre {  get; set; }
+        public Pokemon(int vida, int ataque, string nombre)
+        {
+            this.Vida = vida;
+            this.Ataque = ataque;
+>>>>>>> 8ae0910 (Lab1 batalla pokemon)
         }
     }
   
