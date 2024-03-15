e14506c46dfb2378f5f6db19933c4ddccaded270
Johanaaguero <128259324+Johanaaguero@users.noreply.github.com>
2024-03-15 14:52:58 -0600

    ACT

---
+++ Labs/johana aguero/Pokemon.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace Lab1.AlexVillegas;
+
+public class Pokemon
+{
+    public int Ataque { get; set; }
+
+    public int Vida { get; set; }
+    public string Nombre { get; set; }
+
+ 
+
+    /// <summary>
+    /// Mi clase Pokemon
+    /// </summary>
+    /// <param name="ataque">Esto representa un ataque</param>
+    /// <param name="vida"></param>
+    /// <param name="Nombre"></param>
+    public Pokemon(int ataque, int vida, string Nombre) 
+    { 
+        this.Vida = vida;
+        this.Ataque = ataque;
+    }
+}
