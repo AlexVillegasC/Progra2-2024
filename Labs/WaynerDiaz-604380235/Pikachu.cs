555a0e587827a9d442048ebc2f4740a916b8d967
wayner <morawayner3@gmail.com>
2024-03-16 09:53:07 -0600

    Lab1

---
+++ Labs/WaynerDiaz-604380235/Pikachu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.WaynerDiaz_604380235
+{
+    public class Pikachu : Pokemon
+    {
+        /// <summary>
+        /// Mi clase Pikachu
+        /// </summary>
+        /// <param name="ataque">Representa un ataque</param>
+        /// <param name="vida"></param>
+        /// <param name="nombre"></param>
+        public Pikachu(int ataque, int vida, string nombre) : base(ataque, vida, nombre)
+        {
+        }
+    }
+}
