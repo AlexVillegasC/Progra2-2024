e8cd7926e79fb4bdd35a0a4d593310b1d57ee032
alexvillegasc <alexvillegascarranza@gmail.com>
2024-03-16 10:50:22 -0600

    Lab 2 - Placeholder

--- CLI.Sandbox/Program.cs
+++ CLI.Sandbox/Program.cs
@@ -1,5 +1,12 @@
 ﻿
 
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Lab2.AlexVillegas;
+
+Pikachu pikachu = new Pikachu(40, 100, "Pikachu");
+pikachu.Entrada();
+pikachu.Atacar();
+
+Charizard charizard = new Charizard(80, 100, "Charizard");
+charizard.Entrada();
+charizard.Atacar();
---
+++ Lab2/AlexVillegas-503990937/Pokemon.cs
@@ -0,0 +1,30 @@
+﻿namespace Lab2.AlexVillegas;
+
+public abstract class Pokemon
+{
+    public int Ataque { get; set; }
+    public int Vida { get; set; }
+    public string Nombre { get; set; }
+
+    /// <summary>
+    /// Mi clase Pokemon
+    /// </summary>
+    /// <param name="ataque">Esto representa un ataque</param>
+    /// <param name="vida"></param>
+    /// <param name="Nombre"></param>
+    protected Pokemon(int ataque, int vida, string Nombre)
+    {
+        this.Vida = vida;
+        this.Ataque = ataque;
+        this.Nombre = Nombre;
+    }
+
+    // Metodo Abstracto
+    public abstract void Atacar();
+
+    // Metodo Virtual
+    public virtual void Entrada()
+    {
+        Console.WriteLine("Yo te elijo!");
+    }
+}
