ceaf0a1627da7f808e01dffeb79de362a7e58f37
Bairon <fallasbaltodanobairon@gmail.com>
2024-03-16 23:51:00 -0600

    creacion de una clase abstracta con la cual se hereda y se implementan dos metodos

---
+++ Lab2/Bairon-fallas-118860753/Cuadrado.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.Bairon_fallas;
+
+public class Cuadrado : FiguraGeometrica
+{
+    public float Lado { get; set; }
+    public Cuadrado(string name, float lado) : base(name)
+    {
+        this.Lado = lado;
+    }
+
+    public override void CalcularArea()
+    {
+        float area = Lado * Lado;
+        Console.WriteLine($"Area:{area}");
+    }
+
+    public override void CalcularPerimetro()
+    {
+        float perimetro = 4 * Lado;
+        Console.WriteLine($"Perimetro: {perimetro}");
+    }
+}
+
+
---
+++ Lab2/Bairon-fallas-118860753/FiguraGeometrica.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace Lab2.Bairon_fallas;
+
+public abstract class FiguraGeometrica
+{
+    public string Name { get; set; }
+
+    protected FiguraGeometrica(string name)
+    {
+        this.Name = name;
+    }
+    public abstract void CalcularArea();
+
+    public abstract void CalcularPerimetro();
+
+
+
+
+
+}
