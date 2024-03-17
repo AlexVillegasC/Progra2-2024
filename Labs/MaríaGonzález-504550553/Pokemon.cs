eaa79c451bde723aa3df013d399749112883d2e6
Car301 <carogonzalezjimenez@gmail.com>
2024-03-17 09:25:56 -0600

    Lab1/MaríaGonzález-504550553

---
+++ Labs/MaríaGonzález-504550553/Pokemon.cs
@@ -0,0 +1,23 @@
+﻿namespace Lab1.MaríaGonzález;
+
+public class Pokemon
+{
+    public int Ataque { get; set; }
+
+    public int Vida { get; set; }
+    public string Nombre { get; set; }
+
+    /// <summary>
+    /// Mi clase Pokemon
+    /// </summary>
+    /// <param name="ataque">Esto representa un ataque</param>
+    /// <param name="vida"></param>
+    /// <param name="Nombre"></param>
+    
+
+    public Pokemon(int ataque, int vida, string Nombre)
+    {
+        this.Vida = vida;
+        this.Ataque = ataque;
+    }
+}
