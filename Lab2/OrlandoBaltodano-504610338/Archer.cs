a9fe02b7ada26954d615e3f7cdff7a5d80e96a82
Orlando Baltodano <luis.baltodano.espinoza@est.una.ac.cr>
2024-03-19 12:18:06 -0600

    [Lab2]Orlando Baltodano

---
+++ Lab2/OrlandoBaltodano-504610338/Archer.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Lab2.OrlandoBaltodano;
+
+public class Archer : Sirvientes
+{
+    public Archer(string nombre, int vida, int ataque) : base(nombre, vida, ataque)
+    {
+    }
+    public override void Atacar()
+    {
+        Console.WriteLine("¡I am the bone of my sword. Steel is my body, and fire is my blood. I have created over a thousand blades. Unknown to death, nor known to life. Have withstood pain to create many weapons, yet those hands will never hold anything. So as I pray, Unlimited Blade Works.!");
+    }
+}
