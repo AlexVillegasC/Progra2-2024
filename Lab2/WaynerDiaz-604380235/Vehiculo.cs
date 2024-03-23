cb1563727f8b9c6e5f39f9bdc4ef3fb8de5aae1b
wayner <morawayner3@gmail.com>
2024-03-23 10:33:31 -0600

    -test

---
+++ Lab2/WaynerDiaz-604380235/Vehiculo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.WaynerDiaz_604380235
+{
+    internal class Vehiculo
+    {
+    }
+}
