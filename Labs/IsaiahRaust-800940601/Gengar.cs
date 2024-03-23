ca8f386a616dc1ad52f74b38d1f0a204b3785a66
isaiahraust <raustisaiah@gmail.com>
2024-03-23 09:29:30 -0600

    Displays the winner of the battle

--- Labs/IsaiahRaust-800940601/Gengar.cs
+++ Labs/IsaiahRaust-800940601/Gengar.cs
@@ -2,6 +2,13 @@
 
 public class Gengar : Pokemon
 {
+
+    /// <summary>
+    /// Mi clase Gengar
+    /// </summary>
+    /// <param name="ataque">Esto representa un ataque</param>
+    /// <param name="vida"></param>
+    /// <param name="nombre"></param>
     public Gengar(int ataque, int vida, string nombre) : base(ataque, vida, nombre)
     {
 
