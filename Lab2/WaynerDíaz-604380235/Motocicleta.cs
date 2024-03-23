c4d0bcb27253de16293935678246ab8b35cede3e
wayner <morawayner3@gmail.com>
2024-03-23 10:23:56 -0600

    Lab2

---
+++ Lab2/WaynerDíaz-604380235/Motocicleta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Lab2.WaynerDíaz
+{
+    public class Motocicleta : WaynerDiaz.Vehiculo
+    {
+        public bool TieneSidecar { get; set; }
+
+        public Motocicleta(string marca, string modelo, int año, bool tieneSidecar)
+            : base(marca, modelo, año)
+        {
+            TieneSidecar = tieneSidecar;
+        }
+
+        // Sobreescritura del método MostrarDetalles
+        public override void MostrarDetalles()
+        {
+            base.MostrarDetalles();
+            Console.WriteLine($"Tiene sidecar: {TieneSidecar}");
+        }
+    }
+}
---
+++ Lab2/WaynerDíaz-604380235/Vehiculo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lab2.WaynerDiaz
+{
+    public class Vehiculo
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Año { get; set; }
+
+        public Vehiculo(string marca, string modelo)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Año = DateTime.Now.Year; // Año actual por defecto
+        }
+
+        public Vehiculo(string marca, string modelo, int año)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Año = año;
+        }
+
+        public virtual void MostrarDetalles()
+        {
+            Console.WriteLine($"Marca: {Marca}, Modelo: {Modelo}, Año: {Año}");
+        }
+
+        public void Arrancar()
+        {
+            Console.WriteLine("El vehículo ha arrancado.");
+        }
+
+        public void Arrancar(string mensaje)
+        {
+            Console.WriteLine(mensaje);
+        }
+    }
+}
