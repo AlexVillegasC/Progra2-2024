421ca10f63379eb71c05bac815ccc503da0d2510
Car301 <carogonzalezjimenez@gmail.com>
2024-03-23 21:51:32 -0600

    lab2/MariaGoonzalez-504550553

---
+++ Lab2/MariaGonzalez-504550553/Laptop.cs
@@ -0,0 +1,22 @@
+﻿namespace Lab2.MariaGonzalez
+{
+  
+    public class Laptop : Computadora
+    {
+        private string marca;
+
+        
+        public Laptop(string modelo, int precio, string marca) : base(modelo, precio)
+        {
+            this.marca = marca;
+        }
+
+        
+        public override void Encender()
+        {
+            Console.WriteLine($"La laptop {modelo} de marca {marca} está encendiendo...");
+        }
+
+        
+    }
+}
