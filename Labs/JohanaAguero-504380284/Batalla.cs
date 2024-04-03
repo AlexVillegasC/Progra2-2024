b5047c9d0d73a79c70eb5bc90fd7ed071b616580
Johanaaguero <128259324+Johanaaguero@users.noreply.github.com>
2024-04-02 21:26:36 -0600

    Lab1

---
+++ Labs/JohanaAguero-504380284/Batalla.cs
@@ -0,0 +1,48 @@
+﻿using Lab1.AlexVillegas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.JohanaAguero_504380284
+{
+    public static class Batalla
+    {
+
+        public static string Pelear(Pokemon pok1, Pokemon pok2, bool aplicarReglasPorDebilidad)
+        {
+
+
+            return string.Empty;
+        }
+        public static string Pelear(Pokemon pok1, Pokemon pok2)
+        {
+            bool turnoPok1 = true; // true si es el turno de pok1, false si es el turno de pok2
+
+            while (pok1.Vida > 0 && pok2.Vida > 0)
+            {
+                if (turnoPok1)
+                {
+                    pok2.Vida -= pok1.Ataque;
+                }
+                else
+                {
+                    pok1.Vida -= pok2.Ataque;
+                }
+
+                turnoPok1 = !turnoPok1; // Cambiar de turno
+            }
+
+            if (pok1.Vida <= 0)
+            {
+                return pok2.Nombre;
+            }
+            else
+            {
+                return pok1.Nombre;
+            }
+            return "Ganador";
+        }
+    }
+}
---
+++ Labs/JohanaAguero-504380284/Pokemon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.JohanaAguero_504380284
+{
+        public class Pokemon
+        {
+            public int Ataque { get; set; }
+
+            public int Vida { get; set; }
+            public string Nombre { get; set; }
+
+
+
+            /// <summary>
+            /// Mi clase Pokemon
+            /// </summary>
+            /// <param name="ataque">Esto representa un ataque</param>
+            /// <param name="vida"></param>
+            /// <param name="Nombre"></param>
+            public Pokemon(int ataque, int vida, string Nombre)
+            {
+                this.Vida = vida;
+                this.Ataque = ataque;
+            }
+        }
+
+    }
