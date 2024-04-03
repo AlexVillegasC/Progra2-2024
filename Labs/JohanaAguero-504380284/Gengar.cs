a223968e4ad9e6676c3dfd21d7f4cffbea7aea6b
Johanaaguero <128259324+Johanaaguero@users.noreply.github.com>
2024-04-02 21:26:36 -0600

    Lab1

---
+++ Labs/JohanaAguero-504380284/Gengar.cs
@@ -0,0 +1,20 @@
+﻿using Lab1.AlexVillegas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.JohanaAguero_504380284
+{
+        public class Gengar : Pokemon
+        {
+            public Gengar(int ataque, int vida, string nombre) : base(ataque, vida, nombre)
+            {
+                this.Ataque = ataque;
+                this.Vida = vida;
+                this.Nombre = nombre;
+            }
+        }
+
+    }
---
+++ Labs/JohanaAguero-504380284/Pikachu.cs
@@ -0,0 +1,22 @@
+﻿using Lab1.AlexVillegas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.JohanaAguero_504380284
+{
+    public class Pikachu : Pokemon
+    {
+        /// <summary>
+        /// Mi clase Pikachu
+        /// </summary>
+        /// <param name="ataque">Representa un ataque</param>
+        /// <param name="vida"></param>
+        /// <param name="nombre"></param>
+        public Pikachu(int ataque, int vida, string nombre) : base(ataque, vida, nombre)
+        {
+        }
+    }
+}
