ca86551243e198c874ea3cc07b154971d8006f83
Jhonpy22 <jhonngranadosrojas@gmail.com>
2024-04-06 10:08:03 -0600

    Abstract class

--- Lab2/JhonnRojas-703090697/Medio_de_Transporte.cs
+++ Lab2/JhonnRojas-703090697/Medio_de_Transporte.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿namespace Lab2.JhonnRojas_703090697
 {
     public class Medio_de_Transporte
@@ -29,6 +30,45 @@
 
     }
 }
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.JhonnRojas_703090697
+{
+    public class Medio_de_Transporte
+    {
+        
+        
+            public string Marca { get; set; }
+
+            public string Color { get; set; }
+
+            public int Matricula { get; set; }
+
+            public string Equilibrio_dinamico { get; set; }
+
+
+
+            public Medio_de_Transporte(string Marca, string Color, int Matricula, string Equilibrio_dinamico)
+            {
+                this.Marca = Marca;
+                this.Color = Color;
+                this.Matricula = Matricula;
+                this.Equilibrio_dinamico = Equilibrio_dinamico;
+            }
+
+            public virtual void Realizar_mantenimiento()
+            {
+                Console.WriteLine("mantenimiento general");
+            }
+
+        }
+    }
+>>>>>>> 0ceb716 (Abstract class)
 
 
 
--- Lab2/JhonnRojas-703090697/Moto.cs
+++ Lab2/JhonnRojas-703090697/Moto.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿namespace Lab2.JhonnRojas_703090697
 {
     public class Moto : Medio_de_Transporte
@@ -19,5 +20,35 @@
         }
     }
 
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Lab2.JhonnRojas_703090697.Medio_de_Transporte;
+
+namespace Lab2.JhonnRojas_703090697
+{
+    public class Moto: Medio_de_Transporte
+    {
+        
+        
+            public string Tipo_motocicleta { get; set; }
+
+
+            public Moto(string Marca, string Color, int Matricula, string Equilibrio_dinamico, string Tipo_motocicleta)
+                : base(Marca, Color, Matricula, Equilibrio_dinamico)
+            {
+                this.Tipo_motocicleta = Tipo_motocicleta;
+            }
+
+            public override void Realizar_mantenimiento()
+            {
+                Console.WriteLine("Verificar cadena y los ejes de cada llanta");
+            }
+        }
+    
+>>>>>>> 0ceb716 (Abstract class)
 }
 
