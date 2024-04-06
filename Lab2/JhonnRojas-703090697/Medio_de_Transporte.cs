78fa42eca7be3474d6846b2c7351f06af75623fa
Jhonpy22 <jhonngranadosrojas@gmail.com>
2024-04-06 10:08:03 -0600

    Abstract class

---
+++ Lab2/JhonnRojas-703090697/Medio_de_Transporte.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.JhonnRojas_703090697
+{
+    public class Medio_de_Transporte
+    {
+        
+        
+            public string Marca { get; set; }
+
+            public string Color { get; set; }
+
+            public int Matricula { get; set; }
+
+            public string Equilibrio_dinamico { get; set; }
+
+
+
+            public Medio_de_Transporte(string Marca, string Color, int Matricula, string Equilibrio_dinamico)
+            {
+                this.Marca = Marca;
+                this.Color = Color;
+                this.Matricula = Matricula;
+                this.Equilibrio_dinamico = Equilibrio_dinamico;
+            }
+
+            public virtual void Realizar_mantenimiento()
+            {
+                Console.WriteLine("mantenimiento general");
+            }
+
+        }
+    }
+
+
+
