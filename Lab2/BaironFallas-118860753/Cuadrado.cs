74962b869ffd2e059743df0734a9bf47c5f72f4c
Bairon <fallasbaltodanobairon@gmail.com>
2024-04-06 09:51:33 -0600

    Lab2-clase abstracta con dos metodos.

---
+++ Lab2/BaironFallas-118860753/Cuadrado.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Lab2.BaironFallas_118860753;
+
+public class Cuadrado:FiguraGeometrica
+{
+    public float Lado { get; set; }
+    public Cuadrado(string name, float lado) : base(name)
+    {
+        this.Lado = lado;
+    }
+
+
+    public override void CalcularArea()
+    {
+        float area = Lado * Lado;
+        Console.WriteLine($"Area:{area}");
+    }
+
+    public override void CalcularPerimetro()
+    {
+        float perimetro = 4 * Lado;
+        Console.WriteLine($"Perimetro: {perimetro}");
+    }
+}
+
+
