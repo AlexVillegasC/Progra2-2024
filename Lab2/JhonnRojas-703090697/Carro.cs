96997de3786628a98043ac59f0b03ce5f49ecd57
Jhonpy22 <jhonngranadosrojas@gmail.com>
2024-04-06 10:08:03 -0600

    Abstract class

---
+++ Lab2/JhonnRojas-703090697/Carro.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Lab2.JhonnRojas_703090697.Medio_de_Transporte;
+
+namespace Lab2.JhonnRojas_703090697
+{
+    public class Carro : Medio_de_Transporte
+    {
+        
+        
+            public string Motor_combustion { get; set; }
+
+            public Carro(string Marca, string Color, int Matricula, string Equilibrio_dinamico, string Motor_combustion)
+                   : base(Marca, Color, Matricula, Equilibrio_dinamico)
+            {
+                this.Motor_combustion = Motor_combustion;
+            }
+
+            public override void Realizar_mantenimiento()
+            {
+                Console.WriteLine("Verificar frenos, sistema de refrigeracion y suspencion");
+
+            }
+
+
+        }
+    }
+
+
