21db3c250151ee9a25bf8e04209293daab688901
Jhonpy22 <jhonngranadosrojas@gmail.com>
2024-04-06 10:08:03 -0600

    Abstract class

--- Lab2/JhonnRojas-703090697/Carro.cs
+++ Lab2/JhonnRojas-703090697/Carro.cs
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 ﻿namespace Lab2.JhonnRojas_703090697
 {
     public class Carro : Medio_de_Transporte
@@ -24,6 +25,8 @@
     }
 }
 =======
+=======
+>>>>>>> 0ceb716 (Abstract class)
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,6 +35,7 @@ using System.Threading.Tasks;
 using static Lab2.JhonnRojas_703090697.Medio_de_Transporte;
 
 namespace Lab2.JhonnRojas_703090697
+<<<<<<< HEAD
 =======
 ﻿namespace Lab2.JhonnRojas_703090697
 >>>>>>> 2550663 (Merge with latest Dev changes)
@@ -61,5 +65,29 @@ namespace Lab2.JhonnRojas_703090697
 =======
 }
 >>>>>>> 2550663 (Merge with latest Dev changes)
+=======
+{
+    public class Carro : Medio_de_Transporte
+    {
+        
+        
+            public string Motor_combustion { get; set; }
+
+            public Carro(string Marca, string Color, int Matricula, string Equilibrio_dinamico, string Motor_combustion)
+                   : base(Marca, Color, Matricula, Equilibrio_dinamico)
+            {
+                this.Motor_combustion = Motor_combustion;
+            }
+
+            public override void Realizar_mantenimiento()
+            {
+                Console.WriteLine("Verificar frenos, sistema de refrigeracion y suspencion");
+
+            }
+
+
+        }
+    }
+>>>>>>> 0ceb716 (Abstract class)
 
 
--- Lab2/JhonnRojas-703090697/Medio_de_Transporte.cs
+++ Lab2/JhonnRojas-703090697/Medio_de_Transporte.cs
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 ﻿namespace Lab2.JhonnRojas_703090697
 {
     public class Medio_de_Transporte
@@ -32,6 +33,8 @@
     }
 }
 =======
+=======
+>>>>>>> 0ceb716 (Abstract class)
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -39,6 +42,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Lab2.JhonnRojas_703090697
+<<<<<<< HEAD
 =======
 ﻿namespace Lab2.JhonnRojas_703090697
 >>>>>>> 2550663 (Merge with latest Dev changes)
@@ -76,6 +80,38 @@ namespace Lab2.JhonnRojas_703090697
 =======
 }
 >>>>>>> 2550663 (Merge with latest Dev changes)
+=======
+{
+    public class Medio_de_Transporte
+    {
+        
+        
+            public string Marca { get; set; }
+
+            public string Color { get; set; }
+
+            public int Matricula { get; set; }
+
+            public string Equilibrio_dinamico { get; set; }
+
+
+
+            public Medio_de_Transporte(string Marca, string Color, int Matricula, string Equilibrio_dinamico)
+            {
+                this.Marca = Marca;
+                this.Color = Color;
+                this.Matricula = Matricula;
+                this.Equilibrio_dinamico = Equilibrio_dinamico;
+            }
+
+            public virtual void Realizar_mantenimiento()
+            {
+                Console.WriteLine("mantenimiento general");
+            }
+
+        }
+    }
+>>>>>>> 0ceb716 (Abstract class)
 
 
 
