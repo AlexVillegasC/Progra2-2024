beefde8c984d93e4ff7c4a7a60a070864d58e419
Melina1304 <melina.cabalceta.hernandez@est.una.ac.cr>
2024-04-05 23:20:41 -0500

    Method changes

--- Lab2/MelinaCabalceta-504530616/Gato.cs
+++ Lab2/MelinaCabalceta-504530616/Gato.cs
@@ -1,15 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Lab2.MelinaCabalceta_504530616
 {
     public class Gato : Animal
     {
-        public Gato(string nombre, string color) : base(nombre, color) 
-        {
+        public Gato(string nombre, string color) : base(nombre, color)
+        { 
         }
 
         //Implementacion del metodo abstracto 
