11d878e5fb7e8fc2d996e351102181a2a190eafc
DiegoGutz316 <luis.gutierrez.zuniga@est.una.ac.cr>
2024-04-06 09:35:58 -0600

    Merge with latest Dev chnages

--- Labs/DiegoGutierrez-504560809/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lab1.DiegoGutierrez_504560809
-{
-    internal class Program
-    {
-        Pokemon blaziken = new Blaziken(33, 123, "Blaziken");
-        Pokemon swampert = new Swampert(27, 140, "Swampert");
-
-    }
-}
