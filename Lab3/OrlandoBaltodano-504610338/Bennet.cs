f854aaa56b4d6ae88933ec8fee5852bfc9b14ef2
Orlando Baltodano <luis.baltodano.espinoza@est.una.ac.cr>
2024-04-07 16:58:26 -0600

    [lab3] Orlando-Baltodano

--- Lab3/OrlandoBaltodano-504610338/Bennet.cs
+++ Lab3/OrlandoBaltodano-504610338/Bennet.cs
@@ -1,12 +1,20 @@
 ﻿
 namespace Lab3.OrlandoBaltodano;
 
+<<<<<<< HEAD
 public class Bennet : IHabilidadDefinitiva, IHabilidadElemental
+=======
+public class Bennet : IHabilidadDefinitiva , IHabilidadElemental
+>>>>>>> e45abd7 ([lab3] Orlando-Baltodano)
 {
     public string Nombre { get; set; }
     public int Vida { get; set; }
 
+<<<<<<< HEAD
     protected Bennet(string nombre, int vida)
+=======
+    protected Bennet(string nombre,int vida) 
+>>>>>>> e45abd7 ([lab3] Orlando-Baltodano)
     {
         this.Nombre = nombre;
         this.Vida = vida;
