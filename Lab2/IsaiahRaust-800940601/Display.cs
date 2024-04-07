d33ec96b078da1c2d057176856f22f0f6ee351e9
isaiahraust <raustisaiah@gmail.com>
2024-04-07 15:22:06 -0600

    Entrega Lab2

---
+++ Lab2/IsaiahRaust-800940601/Display.cs
@@ -0,0 +1,19 @@
+﻿namespace Lab2.IsaiahRaust
+{
+    class Display
+    {
+        static void Main(string[] args)
+        {
+            Shapes square = new Square(5);
+            Shapes triangle = new Triangle(4, 3);
+            Shapes circle = new Circle(2);
+
+            Console.WriteLine("Square Area: " + square.CalculateArea());
+            Console.WriteLine("Square Perimeter: " + square.CalculatePerimeter());
+            Console.WriteLine("Triangle Area: " + triangle.CalculateArea());
+            Console.WriteLine("Triangle Perimeter: " + triangle.CalculatePerimeter());
+            Console.WriteLine("Circle Area: " + circle.CalculateArea());
+            Console.WriteLine("Circle Perimeter: " + circle.CalculatePerimeter());
+        }
+    }
+}
