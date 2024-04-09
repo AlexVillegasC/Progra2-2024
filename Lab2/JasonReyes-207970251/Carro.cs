b52dd937475c584238f5c0542a211df7e5ef51f2
Jason Reyes Ocampos <130868181+Jason9783@users.noreply.github.com>
2024-04-08 20:40:20 -0600

    Lab2 final changes

--- Lab2/JasonReyes-207970251/Carro.cs
+++ Lab2/JasonReyes-207970251/Carro.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿namespace Lab2.JasonReyes;
 
 public class Carro : Vehiculo
@@ -9,6 +10,25 @@ public class Carro : Vehiculo
     }
 
 
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.JasonReyes;
+
+public class Carro : Vehiculo
+{
+   
+
+    public Carro(string marca, string modelo, string color): base(marca, modelo, color) 
+    {
+    }
+
+ 
+>>>>>>> e793bcd (Lab2 final changes)
 
     public override void Arrancar()
     {
--- Lab2/JasonReyes-207970251/Motocicleta.cs
+++ Lab2/JasonReyes-207970251/Motocicleta.cs
@@ -1,4 +1,14 @@
+<<<<<<< HEAD
 ﻿namespace Lab2.JasonReyes;
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.JasonReyes;
+>>>>>>> e793bcd (Lab2 final changes)
 
 public class Motocicleta : Vehiculo
 {
