e5a5523080a30f54fe7282611a4a90e6bf9ab0a2
Jason Reyes Ocampos <130868181+Jason9783@users.noreply.github.com>
2024-04-08 20:40:20 -0600

    Lab2 final changes

---
+++ Lab2/JasonReyes-207970251/Vehiculo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.JasonReyes;
+
+public abstract class Vehiculo
+{
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+    public string Color { get; set; }
+
+
+    protected Vehiculo(string marca, string modelo, string color)
+    {
+        this.Marca = marca;
+        this.Modelo = modelo;
+        this.Color = color;
+    }
+
+
+
+
+    public abstract void Arrancar();
+
+    public virtual void Conducir()
+    {
+        Console.WriteLine("Conduciendo...");
+    }
+
+
+}
