939cc181aa868bd007426725833abb72a59506f5
Melina1304 <melina.cabalceta.hernandez@est.una.ac.cr>
2024-04-08 22:17:53 -0500

    Lab3 Interface

---
+++ Lab3/MelinaCabalceta-504530616/IArmaADistancia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3.MelinaCabalceta_504530616
+{
+    public interface IArmaADistancia
+    {
+        void DispararArco();
+
+    }
+}
