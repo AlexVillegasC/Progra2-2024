b0594b7d1fd43be74dea99a56302166026acd22d
FranderCarrillo <frander.carrillo.torres@est.una.ac.cr>
2024-04-09 11:23:53 -0600

    creacion de la clase Pokemon

--- Labs/frander-504440503/Pokemon.cs
+++ Labs/frander-504440503/Pokemon.cs
@@ -1,12 +1,10 @@
-public class Pokemon
-{
+public class Pokemon{
     public int Ataque { get; set; }
 
     public int Vida { get; set; }
     public string Nombre { get; set; }
     
-    public Pokemon(int ataque, int vida, string Nombre) 
-    { 
+    public Pokemon(int ataque, int vida, string Nombre) { 
         this.Vida = vida;
         this.Ataque = ataque;
         this.Nombre = Nombre;
