28e7035effb0a2487155277fbc810d274eb202b9
Jason Reyes Ocampos <130868181+Jason9783@users.noreply.github.com>
2024-04-08 20:40:20 -0600

    Lab2 final changes

---
+++ Lab2/JasonReyes-207970251/Carro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.JasonReyes;
+
+public class Carro : Vehiculo
+{
+   
+
+    public Carro(string marca, string modelo, string color): base(marca, modelo, color) 
+    {
+    }
+
+ 
+
+    public override void Arrancar()
+    {
+        Console.WriteLine("Encendiendo el carro...");
+    }
+}
