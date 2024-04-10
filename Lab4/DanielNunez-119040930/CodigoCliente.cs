f27fe80ff273f766a419876bb8bf9996c4d70027
SoyMaloLoSeShh <jose.nunez.villalobos@est.una.cr>
2024-04-09 20:28:01 -0600

    testing

---
+++ Lab4/DanielNunez-119040930/CodigoCliente.cs
@@ -0,0 +1,8 @@
+﻿using Lab4.DanielNunez_119040930;
+
+ContenedorDaniel<Piezas> miContenedorDeWish = new Lab4.DanielNunez_119040930.ContenedorDaniel<Piezas>();
+Piezas peon = new Piezas("Peon");
+Piezas dama = new Piezas("Dama");
+miContenedorDeWish.Agregar(peon);
+miContenedorDeWish.Agregar(dama);
+miContenedorDeWish.MostrarElementos();
