5e38bea633f11cf83eeae6066a978199b55896b8
SoyMaloLoSeShh <jose.nunez.villalobos@est.una.cr>
2024-04-09 18:58:32 -0600

    testing

--- Lab3/DanielNunez-119040930/TelefonoConBateria.cs
+++ Lab3/DanielNunez-119040930/TelefonoConBateria.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//
 namespace Lab3.DanielNunez_119040930
 {
     public interface TelefonoConBateria
