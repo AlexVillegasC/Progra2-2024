6fdee9bb7d67d8ea2f3dba9e054a60ba66947236
heyitskarinamd@outlook.com <heyitskarinamd@outlook.com>
2024-04-09 19:45:36 -0600

    lab 3 y 4

---
+++ Lab4/KarinaMoreno-504520784/Lab4-ClientKarina.cs
@@ -0,0 +1,7 @@
+﻿using Lab4.KarinaMoreno_504520784;
+using Lab4.KarinaMoreno_504520784.Decoration;
+
+ContenedorKarina<Plushies> HomeDecor = new Lab4.KarinaMoreno_504520784.ContenedorKarina<Plushies>();
+Plushies cinnamoroll = new Plushies("cinnamoroll");
+HomeDecor.agregar(cinnamoroll);
+HomeDecor.mostrarObjetos();
