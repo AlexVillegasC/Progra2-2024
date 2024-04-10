cd6797b6eee1270e19f80d46043936bc1a28a785
SoyMaloLoSeShh <jose.nunez.villalobos@est.una.cr>
2024-04-09 18:57:19 -0600

    Testing

---
+++ Lab3/DanielNunez-119040930/Telefono.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3.DanielNunez_119040930
+{
+    public class Telefono : TelefonoConBateria, TelefonoConConexionInalabrica
+
+    {
+        public void TurnOn()
+        {
+            Console.WriteLine("Encendiendo el teléfono móvil...");
+        }
+
+        public void TurnOff()
+        {
+            Console.WriteLine("Apagando el teléfono móvil...");
+        }
+
+        public void Connect()
+        {
+            Console.WriteLine("Conectando el teléfono móvil a la red...");
+        }
+
+        public void Disconnect()
+        {
+            Console.WriteLine("Desconectando el teléfono móvil de la red...");
+        }
+    }
+}
