054dc758f637cbed8a11e5da27bc5fae33f5a9ce
SoyMaloLoSeShh <jose.nunez.villalobos@est.una.cr>
2024-04-09 20:28:01 -0600

    testing

---
+++ Lab4/DanielNunez-119040930/ContenedorDaniel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4.DanielNunez_119040930
+{
+    public class ContenedorDaniel<T>
+    {
+        private List<T> elementos = new List<T>(); // Lista para almacenar elementos de cualquier tipo
+
+        // Método para agregar elementos al contenedor
+        public void Agregar(T elemento)
+        {
+            elementos.Add(elemento);
+        }
+
+        // Método para mostrar los elementos del contenedor
+        public void MostrarElementos()
+        {
+            Console.WriteLine("Elementos en el contenedor:");
+            foreach (var elemento in elementos)
+            {
+                Console.WriteLine(elemento);
+            }
+        }
+    }
+}
