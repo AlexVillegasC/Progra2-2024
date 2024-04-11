49ff694e328f15d7a55c0930d53e34d3e77551fa
DannyOr94 <danny.abarca.alvarado@est.una.ac.cr>
2024-04-11 12:27:16 -0600

    Lab1 Danny Abarca 504000385

---
+++ Labs/Danny-Abarca-504000385/Psyduck.cs
@@ -0,0 +1,14 @@
+﻿namespace Lab1.Danny_Abarca_504000385
+{
+    internal class Psyduck : Pokemon
+    {
+        public Psyduck(int ataque, int vida, int defensa, string nombre) : base(ataque, vida, defensa, nombre)
+        {
+            this.Ataque = ataque;
+            this.Vida = vida;
+            this.Defensa = defensa;
+            this.Nombre = nombre;
+        }
+
+    }
+}
