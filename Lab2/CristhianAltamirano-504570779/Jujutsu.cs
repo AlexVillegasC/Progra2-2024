b97d4a6d7d9c94d0fe5d7abd0b6493b81a8b3d50
crissXcoder <cristhian.altamirano.montes@est.una.ac.cr>
2024-04-11 12:48:16 -0600

    Lab3-CristhianAltamirano-504570779

---
+++ Lab2/CristhianAltamirano-504570779/Jujutsu.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Lab2.CristhianAltamirano;
+public abstract class Jujutsu
+{
+    public int Ataque { get; set; }
+    public int Vida { get; set; }
+    public string Nombre { get; set; }
+    public int Dominio { get; set; }
+
+    protected Jujutsu(int ataque, int vida, string Nombre, int dominio)
+    {
+        this.Vida = vida;
+        this.Ataque = ataque;
+        this.Nombre = Nombre;
+        this.Dominio = dominio;
+    }
+
+    public abstract void ExpansionDeDominio();
+
+    public virtual void Entrada()
+    {
+        Console.WriteLine("La batalla entre los dos mas fuertes ha comenzado!");
+    }
+}
+
