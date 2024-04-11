570bac3dba4e4c930d77105e21320693fece3e70
crissXcoder <cristhian.altamirano.montes@est.una.ac.cr>
2024-04-11 12:48:16 -0600

    Lab3-CristhianAltamirano-504570779

--- Lab2/CristhianAltamirano-504570779/Gojo.cs
+++ Lab2/CristhianAltamirano-504570779/Gojo.cs
@@ -2,6 +2,7 @@
 namespace Lab2.CristhianAltamirano;
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 public class Gojo : Jujutsu
 {
     public Gojo(int ataque, int vida, string Nombre, int dominio) : base(ataque, vida, Nombre, dominio)
@@ -40,3 +41,20 @@ public class Gojo : Jujutsu
 }
 
 >>>>>>> 2550663 (Merge with latest Dev changes)
+=======
+    public class Gojo : Jujutsu
+    {
+        public Gojo(int ataque, int vida, string Nombre, int dominio) : base(ataque, vida, Nombre, dominio)
+        {
+        }
+
+        public override void ExpansionDeDominio()
+        {
+            Console.WriteLine("Azul");
+            Console.WriteLine("Rojo");
+            Console.WriteLine("Ritual de vacio.... Purpura.");
+            Console.WriteLine("Nah, i'd win!");
+        }
+    }
+
+>>>>>>> b97d4a6 (Lab3-CristhianAltamirano-504570779)
