bffd73945fc39c638452967ede009a550f57a671
alexvillegasc <alexvillegascarranza@gmail.com>
2024-04-11 21:42:13 -0600

    lab3 Karina - Limpiando cambios de Lab3

--- Lab4/KarinaMoreno-504520784/ContenedorKarina.cs
+++ Lab4/KarinaMoreno-504520784/ContenedorKarina.cs
@@ -1,4 +1,6 @@
-﻿namespace Lab4.KarinaMoreno_504520784;
+﻿using Lab4.KarinaMoreno_504520784.Decoration;
+
+namespace Lab4.KarinaMoreno_504520784;
 
 public class ContenedorKarina<T>
 {
@@ -18,3 +20,12 @@ public class ContenedorKarina<T>
         }
     }
 }
+
+// CODIGO para el ejecutar en el Program.cs
+//using Lab4.KarinaMoreno_504520784;
+//using Lab4.KarinaMoreno_504520784.Decoration;
+
+//ContenedorKarina<Plushies> HomeDecor = new Lab4.KarinaMoreno_504520784.ContenedorKarina<Plushies>();
+//Plushies cinnamoroll = new Plushies("cinnamoroll");
+//HomeDecor.agregar(cinnamoroll);
+//HomeDecor.mostrarObjetos();
