7d6dc72201605485bf4c802284c4ad15f2e9f6cf
alexvillegasc <alexvillegascarranza@gmail.com>
2024-04-11 21:42:13 -0600

    lab3 Karina - Limpiando cambios de Lab3

--- Lab4/KarinaMoreno-504520784/Lab4-ClientKarina.cs
+++
@@ -1,7 +0,0 @@
-﻿using Lab4.KarinaMoreno_504520784;
-using Lab4.KarinaMoreno_504520784.Decoration;
-
-ContenedorKarina<Plushies> HomeDecor = new Lab4.KarinaMoreno_504520784.ContenedorKarina<Plushies>();
-Plushies cinnamoroll = new Plushies("cinnamoroll");
-HomeDecor.agregar(cinnamoroll);
-HomeDecor.mostrarObjetos();
