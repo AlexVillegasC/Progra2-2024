cd7a174b07089d94b95bd9096761dfc839f65787
Johanaaguero <128259324+Johanaaguero@users.noreply.github.com>
2024-04-13 10:58:50 -0600

    Lab4-Johana-Aguero-504380284

---
+++ Lab4/JohanaAguero-504380284/Libro.cs
@@ -0,0 +1,14 @@
+﻿
+namespace lab4.JohanaAguero;
+
+class Libro
+{
+    public string Titulo { get; set; }
+    public string Autor { get; set; }
+
+    public Libro(string titulo, string autor)
+    {
+        Titulo = titulo;
+        Autor = autor;
+    }
+}
