40bffef3c6946c53425399ce3a5ebb2acb438623
Car301 <carogonzalezjimenez@gmail.com>
2024-04-13 11:07:03 -0600

    Lab4/MariaGonzalez-504550553

---
+++ Lab4/MariaGonzalez-504550553/Celular.cs
@@ -0,0 +1,15 @@
+﻿namespace Lab4.MariaGonzalez;
+
+public class Celular
+{
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+    public decimal Precio { get; set; }
+
+    public Celular(string marca, string modelo, decimal precio)
+    {
+        Marca = marca;
+        Modelo = modelo;
+        Precio = precio;
+    }
+}
