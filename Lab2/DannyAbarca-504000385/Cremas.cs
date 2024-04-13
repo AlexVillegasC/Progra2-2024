1ae5db30c51871f2c90af162bbc4f52d4a189a10
DannyOr94 <danny.abarca.alvarado@est.una.ac.cr>
2024-04-13 11:01:46 -0600

    Cambio del nombre de las clases

---
+++ Lab2/DannyAbarca-504000385/Cremas.cs
@@ -0,0 +1,16 @@
+﻿using Lab2.DannyAbarca;
+
+namespace Lab3.DannyAbarca
+{
+    public class Cremas : ProductosCanabicos
+    {
+        public Cremas(string NombreArticulo, int NivelTHC, int NivelCBD, int Precio) : base(NombreArticulo, NivelTHC, NivelCBD, Precio)
+        {
+        }
+
+        public override void TipodeArticulo()
+        {
+            Console.WriteLine("En esta seccion se encontraran diversas cremas corporales a base de canabis, estas permitiran ayudarte a aliviar dolencias corporales");
+        }
+    }
+}
--- Labs/Danny-Abarca-504000385/Pokemon2.cs
+++ Labs/Danny-Abarca-504000385/Pokemon2.cs
@@ -1,14 +1,13 @@
 ﻿namespace Lab1.Danny_Abarca_504000385
 {
-    internal class Psyduck : Pokemon
+    internal class Pokemon2 : Pokemon
     {
-        public Psyduck(int ataque, int vida, int defensa, string nombre) : base(ataque, vida, defensa, nombre)
+        public Pokemon2(int ataque, int vida, int defensa, string nombre) : base(ataque, vida, defensa, nombre)
         {
             this.Ataque = ataque;
             this.Vida = vida;
             this.Defensa = defensa;
             this.Nombre = nombre;
         }
-
     }
 }
