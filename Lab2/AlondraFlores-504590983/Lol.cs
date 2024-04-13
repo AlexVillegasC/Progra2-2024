0af365b8306d98853179dfb25292082222c75393
ALONDRA ESPINOZA FLORES <alondra.espinoza.flores@est.una.ac.cr>
2024-04-13 12:35:01 -0600

    Laboratorio 2,3 y 4

--- Lab2/AlondraFlores-504590983/Lol.cs
+++ Lab2/AlondraFlores-504590983/Lol.cs
@@ -21,4 +21,5 @@ public abstract class Lol
         Console.WriteLine("Bienvenido a la Grieta del invocador");
     }
 
+
 }
--- Lab2/AlondraFlores-504590983/Yasuo.cs
+++ Lab2/AlondraFlores-504590983/Yasuo.cs
@@ -11,5 +11,6 @@ public class Yasuo : Lol
         Console.WriteLine("Tirar Segunda Habilidad");
     }
 
+
 }
 
