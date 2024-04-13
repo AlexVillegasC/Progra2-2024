208a3dec7882b7f37a4f1bc17687a91da73ae47f
alexvillegasc <alexvillegascarranza@gmail.com>
2024-04-12 23:34:40 -0600

    Lab5 - Setup

--- CLI.Sandbox/Program.cs
+++ CLI.Sandbox/Program.cs
@@ -1,14 +1,4 @@
 ﻿
 
 
-using Lab4.AlexVillegas_503990937;
-using Lab4.AlexVillegas_503990937.Productos;
-
-ContenedorAlexV<Muebles> miContenedorDeWish = new Lab4.AlexVillegas_503990937.ContenedorAlexV<Muebles>();
-Muebles sillon = new Muebles("Sillon");
-Muebles mesa = new Muebles("Mesa");
-miContenedorDeWish.Agregar(sillon);
-miContenedorDeWish.Agregar(mesa);
-
-
-miContenedorDeWish.MostrarElementos();
+// Usar solo para pruebas!
--- Lab4/DanielNunez-119040930/CodigoCliente.cs
+++ Lab4/DanielNunez-119040930/CodigoCliente.cs
@@ -1,10 +1,10 @@
 ﻿
 
-using Lab4.DanielNunez_119040930;
+//using Lab4.DanielNunez_119040930;
 
-ContenedorDaniel<Piezas> miContenedorDeWish = new Lab4.DanielNunez_119040930.ContenedorDaniel<Piezas>();
-Piezas peon = new Piezas("Peon");
-Piezas dama = new Piezas("Dama");
-miContenedorDeWish.Agregar(peon);
-miContenedorDeWish.Agregar(dama);
-miContenedorDeWish.MostrarElementos();
+//ContenedorDaniel<Piezas> miContenedorDeWish = new Lab4.DanielNunez_119040930.ContenedorDaniel<Piezas>();
+//Piezas peon = new Piezas("Peon");
+//Piezas dama = new Piezas("Dama");
+//miContenedorDeWish.Agregar(peon);
+//miContenedorDeWish.Agregar(dama);
+//miContenedorDeWish.MostrarElementos();
