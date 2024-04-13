eefcaf15e5935989ea06d966e2219d63d777ae5b
Axxxxxel11 <128259347+Axxxxxel11@users.noreply.github.com>
2024-04-13 11:51:20 -0600

    local fix

--- CLI.Sandbox/Program.cs
+++ CLI.Sandbox/Program.cs
@@ -1,29 +1,5 @@
-﻿using Lab1.AxelBadilla_504430558;
-
-Pokemon pikachu = new Pikachu("Pikachu",100, 60);
-Pokemon greninja = new Greninja("Greninja",100, 70);
-
-var batalla = new Lab1.AxelBadilla_504430558.Batalla();
-batalla.Batallar(pikachu, greninja);
+﻿
 
 
-
-
-
-
-
-
-
-
-
-
-//using Lab4.AlexVillegas_503990937.Productos;
-//using Lab4.AlexVillegas_503990937;
-
-//ContenedorAlexV<Muebles> miContenedorDeWish = new Lab4.AlexVillegas_503990937.ContenedorAlexV<Muebles>();
-//Muebles sillon = new Muebles("Sillon");
-//Muebles mesa = new Muebles("Mesa");
-//miContenedorDeWish.Agregar(sillon);
-//miContenedorDeWish.Agregar(mesa);
-//miContenedorDeWish.MostrarElementos();
-
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
