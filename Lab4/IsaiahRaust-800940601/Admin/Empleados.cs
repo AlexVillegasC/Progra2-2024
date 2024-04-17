fcef6089b204d94d7bc48994bbdf43074d430a94
isaiahraust <raustisaiah@gmail.com>
2024-04-17 00:49:57 -0600

    Lab4 partial

---
+++ Lab4/IsaiahRaust-800940601/Admin/Empleados.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4.IsaiahRaust_800940601.Admin
+{
+    internal class Empleados
+    {
+    }
+}
