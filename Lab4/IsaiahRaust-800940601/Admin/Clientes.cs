c53b0fb2ae48135f9f6bd020f31168c00d81bd60
isaiahraust <raustisaiah@gmail.com>
2024-04-17 00:39:18 -0600

    Lab4 completado

---
+++ Lab4/IsaiahRaust-800940601/Admin/Clientes.cs
@@ -0,0 +1,17 @@
+﻿namespace Lab4.IsaiahRaust.Admin
+{
+    public class Clientes
+    {
+
+        public string Nombre { get; set; }
+
+        public Clientes(string nombre) { Nombre = nombre; }
+
+        public override string ToString()
+        {
+            return $"Nombre: {Nombre}";
+        }
+
+    }
+
+}
---
+++ Lab4/IsaiahRaust-800940601/Admin/Empleados.cs
@@ -0,0 +1,17 @@
+﻿namespace Lab4.IsaiahRaust.Admin
+{
+    public class Empleados
+    {
+
+        public string Codigo { get; set; }
+
+        public Empleados(string codigo) { Codigo = codigo; }
+
+        public override string ToString()
+        {
+            return $"Codigo de empleado: {Codigo}";
+        }
+
+    }
+
+}
