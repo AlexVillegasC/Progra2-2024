43a1cc7169d02e70c403b7f5de477c3dd977572c
alexvillegasc <alexvillegascarranza@gmail.com>
2024-04-20 01:02:26 -0600

    Lab 5

---
+++ Lab5/AlexVillegas-503990937/Composicion/Corazon.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab5.AlexVillegas_503990937.Composicion;
+public class Corazon
+{
+    public int LatidosPorMinuto { get; set; }
+
+    public Corazon()
+    {
+        LatidosPorMinuto = 70;
+    }
+}
