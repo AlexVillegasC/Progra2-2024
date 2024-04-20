3caf6daf7a2c4cba470c889da6b0d22bf58538d5
Orlando Baltodano <luis.baltodano.espinoza@est.una.ac.cr>
2024-04-20 11:17:01 -0600

    [Lab5-Orlando-Baltodano-504610338]

---
+++ Lab5/OrlandoBaltodano-504610338/Agregacion/Empresa.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Lab5.OrlandoBaltodano.Agregacion;
+
+internal class Empresa
+{
+    public List<Empleado> Empresas { get; set; }
+
+    public Empresa()
+    {
+        Empresas = new List<Empleado>();
+    }
+    public void AgregarEmpleado(Empleado empleado)
+    {
+        Empresas.Add(empleado);
+    }
+
+
+}
