9946b720b51cce8098660affd946e0f918c2bc6b
Brayanperezballadares <bpballadares@gmail.com>
2024-04-20 11:41:11 -0600

    lab1 Brayan

---
+++ Labs/BrayanPérez-504510529/Pokemon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.BrayanPérez_504510529
+{
+    public class Pokemon
+    {
+        public int Vida { get; set; } = 100;
+
+        public int Ataque { get; set; }
+
+        public string Nombre { get; set; }
+
+
+        public Pokemon(int vida, int ataque, string nombre)
+        {
+            this.Vida = vida;
+            this.Nombre = nombre;
+
+
+        }
+    }
+}
