467111d8d53aaf41c694682255082e49fc4c61d1
ALONDRA ESPINOZA FLORES <alondra.espinoza.flores@est.una.ac.cr>
2024-04-22 18:06:22 -0600

    Lab5

---
+++ Lab5/AlondraFlores-504590983/Agregacion/TransportesLaPampa.cs
@@ -0,0 +1,16 @@
+﻿namespace Lab5.AlondraFlores_504590983.Agregacion;
+
+public class TransportesLaPampa
+{
+    public List<Vehiculo> vehiculos { get; set;}
+
+    public TransportesLaPampa()
+    {
+        vehiculos = new List<Vehiculo>();
+    }
+
+    public void AgregarVehiculo(Vehiculo vehiculo)
+    {
+        vehiculos.Add(vehiculo);
+    }
+}
---
+++ Lab5/AlondraFlores-504590983/Composicion/Sol.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab5.AlondraFlores_504590983.Composicion;
+
+public class Sol
+{
+    public string Fotosintesis { get; set; }  //atributo
+
+    public Sol()
+    {
+        Fotosintesis = "Recibir luz solar diaria";  //constructor
+    }
+}
