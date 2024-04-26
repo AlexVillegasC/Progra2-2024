93000881b21578a07d31d279c1339c2ca4961350
wayner <morawayner3@gmail.com>
2024-04-26 17:19:41 -0600

    Lab5 WaynerDiaz

---
+++ Lab5/WaynerDiaz-604380235/Agregacion/Producto.cs
@@ -0,0 +1,6 @@
+namespace Lab5.WaynerDiaz;
+class Producto
+{
+    public string Nombre { get; set; }
+    public decimal Precio { get; set; }
+}
---
+++ Lab5/WaynerDiaz-604380235/Composicion/TelefonoMovil.cs
@@ -0,0 +1,7 @@
+namespace Lab5.WaynerDiaz;
+class TelefonoMovil
+{
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+    public decimal Precio { get; set; }
+}
