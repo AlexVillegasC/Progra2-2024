4000d1c1aa5faf7be2900a08e99531396eef0399
Johanaaguero <128259324+Johanaaguero@users.noreply.github.com>
2024-04-26 17:21:03 -0600

    Lab5-JohanaAguero

---
+++ Lab5/JohanaAguero-504380284/Agregacion/Museo.cs
@@ -0,0 +1,18 @@
+﻿namespace Lab5.JohanaAguero;
+
+class Museo
+{
+    public string Nombre { get; set; }
+    public List<Artículo> Colección { get; set; }
+
+    public Museo(string nombre)
+    {
+        Nombre = nombre;
+        Colección = new List<Artículo>();
+    }
+
+    public void AgregarArtículo(Artículo artículo)
+    {
+        Colección.Add(artículo);
+    }
+}
