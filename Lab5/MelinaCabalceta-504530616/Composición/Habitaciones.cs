1d78070bdc9efe064fe5ca35c772a410c7cc14f4
Melina1304 <melina.cabalceta.hernandez@est.una.ac.cr>
2024-04-27 09:14:59 -0500

    Test

---
+++ Lab5/MelinaCabalceta-504530616/Composición/Habitaciones.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5.MelinaCabalceta_504530616.Composición
+{
+    public class Habitaciones
+    {
+        public int numeroHabitacion {  get; set; }
+    }
+}
