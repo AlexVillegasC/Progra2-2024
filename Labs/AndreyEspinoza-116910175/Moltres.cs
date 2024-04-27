93b0be9652e03de6f4cbae79d50d20ee60f58b08
Andrey <andrey.espinoza.juarez@est.una.ac.cr>
2024-04-27 09:17:58 -0600

    lab 5 y lab1

---
+++ Labs/AndreyEspinoza-116910175/Moltres.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Lab1.AndreyEspinoza_116910175
+{
+    public class Moltres : Pokemon
+    {
+        public Moltres(int vida, int ataque, string name) : base (vida, ataque, name)
+    
+        { }
+
+        public int Vida {  get; internal set; }
+        
+        public int Ataque { get; internal set; }
+
+        public static implicit operator Moltres(bool v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
