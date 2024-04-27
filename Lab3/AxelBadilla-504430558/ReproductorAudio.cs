434a816e113fef0bf5b13bf270846825e430b8d5
Axxxxxel11 <128259347+Axxxxxel11@users.noreply.github.com>
2024-04-27 12:42:16 -0600

    lab3 AxelBadilla

---
+++ Lab3/AxelBadilla-504430558/ReproductorAudio.cs
@@ -0,0 +1,34 @@
+﻿using Lab3.AxelBadilla_504430558;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3.AxelBadilla_504430558
+{
+    public class ReproductorAudio : IReproductorMultimedia, IDetenerReproduccion
+    {
+        public void Reproducir()
+        {
+            Console.WriteLine("Reproduciendo Audio...");
+        }
+
+
+        public void Detener()
+        {
+
+            Console.WriteLine("Deteniendo reproduccion...");
+        }
+    }
+}
+
+
+
+//ReproductorAudio reproductoraudio = new ReproductorAudio();
+//reproductoraudio.Reproducir();
+//reproductoraudio.Detener();
+
+//ReproductorVideo reproductorvideo = new ReproductorVideo();
+//reproductorvideo.Reproducir();
+//reproductorvideo.Detener();
