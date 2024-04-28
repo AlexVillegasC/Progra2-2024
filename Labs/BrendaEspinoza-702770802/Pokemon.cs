633063df9d3f77f9157c5f859d611315c48e079c
Brenda <brenda.espinoza.matarrita@est.una.ac.cr>
2024-04-27 20:28:12 -0600

    Laboratorio1

---
+++ Labs/BrendaEspinoza-702770802/Pokemon.cs
@@ -0,0 +1,21 @@
+namespace Lab1.BrendaEspinoza;
+
+public class Pokemon{
+    public int Vida { get; set;}
+    public int Ataque { get; set;}
+    public string Nombre { get; set;}
+ public Pokemon(int ataque, int vida, string Nombre)
+    {
+        this.Vida = vida;
+        this.Ataque = ataque;
+        this.Nombre = Nombre;
+    }
+
+    public void Danio(int danio){
+        this.Vida -= danio;
+    }
+
+    public bool EstaVivo(){
+       return this.Vida > 0;
+    }
+}
