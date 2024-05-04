4ee27bbac850be0abcc952d971f3d00c1ffd315c
Axxxxxel11 <128259347+Axxxxxel11@users.noreply.github.com>
2024-05-04 12:05:39 -0600

    creacion de clase mantenimiento

--- NLayer.Architecture.Bussines.Models/ControlMaquinaria/Mantenimiento.cs
+++ NLayer.Architecture.Bussines.Models/ControlMaquinaria/Mantenimiento.cs
@@ -1,19 +1,25 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 ﻿
 
 namespace NLayer.Architecture.Bussines.ReporteInventario;
 =======
+=======
+>>>>>>> 773e039 (creacion de clase mantenimiento)
 ﻿using System;
 
 
 namespace NLayer.Architecture.Bussines.Models.ControlMaquinaria;
+<<<<<<< HEAD
 >>>>>>> 773e039 (creacion de clase mantenimiento)
 =======
 ﻿
 
 namespace NLayer.Architecture.Bussines.ReporteInventario;
 >>>>>>> 4b4602e (Creacion de los servicios, interfaces,json de la clase mantenimiento)
+=======
+>>>>>>> 773e039 (creacion de clase mantenimiento)
 
 public class Mantenimiento
 {
