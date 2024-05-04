9b18fc106017e21fd23903a281278b20ff2b882a
Axxxxxel11 <128259347+Axxxxxel11@users.noreply.github.com>
2024-05-04 08:22:54 -0600

    lab5 Axel Badilla

---
+++ Lab5/AxelBadilla-504430558/Prueba.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5.AxelBadilla_504430558
+{
+    public class Prueba
+    {
+
+        //Universidad departamento=new Universidad();
+
+        //Universidad.Agregar_departamento();
+        //Universidad.ImprimirDepartamento();
+
+
+    }
+}
