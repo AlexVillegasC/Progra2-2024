7d38d1b766bbc9c530623483e65668f5dfefd250
Axxxxxel11 <128259347+Axxxxxel11@users.noreply.github.com>
2024-05-04 12:05:39 -0600

    creacion de clase mantenimiento

---
+++ NLayer.Architecture.Bussines.Models/ControlMaquinaria/Mantenimiento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace NLayer.Architecture.Bussines.Models.ControlMaquinaria;
+
+public class Mantenimiento
+{
+
+    public string Nombre { get; set; }
+
+    public int Id { get; set; }
+
+    public string Direccion { get; set; }
+
+    public int Telefono { get; set; }
+
+    public string Correo  { get; set; }
+
+}
