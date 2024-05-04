ffb6c523c7ff30d5baf7465ffec1369fadbbe73e
Orlando Baltodano <luis.baltodano.espinoza@est.una.ac.cr>
2024-05-04 17:21:44 -0600

    Trabajadores

---
+++ NLayer.Architecture.Bussines.Models/Alimentacion_Nutricion/Produccion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Architecture.Bussines.Models.Alimentacion_Nutricion
+{
+    internal class Produccion
+    {
+    }
+}
---
+++ NLayer.Architecture.Bussines.Models/Alimentacion_Nutricion/Trabajadores.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Architecture.Bussines.Models.Alimentacion_Nutricion
+{
+    public class Trabajadores
+    {
+        public string Nombre { get; set; }
+        public double Costo_Planilla {  get; set; }
+    }
+}
