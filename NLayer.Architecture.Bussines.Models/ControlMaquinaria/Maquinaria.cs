10d5d9caf2445e8eeaa1a6d24a1eb6e009343cfd
Bairon <fallasbaltodanobairon@gmail.com>
2024-05-04 11:39:31 -0600

    Creacion de la carpeta para las clases

--- NLayer.Architecture.Bussines.Models/ControlMaquinaria/Maquinaria.cs
+++ NLayer.Architecture.Bussines.Models/ControlMaquinaria/Maquinaria.cs
@@ -6,6 +6,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 ﻿
 namespace NLayer.Architecture.Bussines.ReporteInventario;
 
@@ -13,6 +14,8 @@ public class Maquinaria
 {
     public string Nombre { get; set; }
 =======
+=======
+>>>>>>> befb4d4 (Creacion de la carpeta para las clases)
 =======
 >>>>>>> befb4d4 (Creacion de la carpeta para las clases)
 =======
@@ -29,6 +32,7 @@ public class Maquinaria
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> befb4d4 (Creacion de la carpeta para las clases)
 =======
 ﻿
@@ -68,6 +72,8 @@ public class Maquinaria
 {
     public string Nombre { get; set; }
 >>>>>>> b9342b3 (Creacion de los servicios de maquinaria)
+=======
+>>>>>>> befb4d4 (Creacion de la carpeta para las clases)
     public string Marca { get; set; }
     public float Precio { get; set; }
     public int Potencia { get; set; }
