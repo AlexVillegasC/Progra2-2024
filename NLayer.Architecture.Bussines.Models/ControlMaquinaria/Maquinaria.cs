16a6fe7f7d7cb0995d17cdb89e0f535e0a993a78
Bairon <fallasbaltodanobairon@gmail.com>
2024-05-04 11:39:31 -0600

    Creacion de la carpeta para las clases

---
+++ NLayer.Architecture.Bussines.Models/ControlMaquinaria/Maquinaria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace NLayer.Architecture.Bussines.Models.ControlMaquinaria;
+
+public class Maquinaria
+{
+    public string Marca { get; set; }
+    public float Precio { get; set; }
+    public int Potencia { get; set; }
+    public float Tamaño { get; set; }
+    public float Peso { get; set; }
+    public string Funcionalidad { get; set; }
+}
