6bf91c0cc23c0b155d472211765b69d44b969de3
Alondra Flores <alondraespinoza897@gmail.com>
2024-05-05 12:01:24 -0600

    Clases y Archivo JSON de Plagas

--- NLayer.Architecture.Bussines.Models/Plantaciones/GestionPlantaciones.cs
+++ NLayer.Architecture.Bussines.Models/Plantaciones/GestionPlantaciones.cs
@@ -1,22 +1,4 @@
-<<<<<<< HEAD
-﻿namespace NLayer.Architecture.Bussines.GestionPlantaciones;
-
-public class GestionPlantaciones
-{
-    public string Nombre { get; set; } = "Reporte de Plantaciones";
-
-    public ControlPlagas Plagas { get; set;}
-
-
-    public ControlAbono Abono { get; set; }
-
-    public ArbolFrutal Arbol { get; set; }
-
-}
-
-
-=======
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
