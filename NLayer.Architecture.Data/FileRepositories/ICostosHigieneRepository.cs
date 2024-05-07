4fd0db3a76bf3d1700ca3a25404018d5b210a13e
heyitskarinamd@outlook.com <heyitskarinamd@outlook.com>
2024-05-07 16:00:12 -0600

    costos higiene

---
+++ NLayer.Architecture.Data/FileRepositories/ICostosHigieneRepository.cs
@@ -0,0 +1,8 @@
+﻿using NLayer.Architecture.Bussines.Models.The_Sillies;
+
+namespace DataAccess.Layer.FileRepositories;
+
+public interface ICostosHigieneRepository
+{
+    Task<CostosHigiene> GetCostosHigiene();
+}
