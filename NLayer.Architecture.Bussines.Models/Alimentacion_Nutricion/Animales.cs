dcedeb4525f69ccfb9a0b8d367871b455dc1225e
Andrey <andrey.espinoza.juarez@est.una.ac.cr>
2024-05-07 16:35:41 -0600

    Subiendo...

---
+++ NLayer.Architecture.Bussines.Models/Alimentacion_Nutricion/Animales.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Architecture.Bussines.Models.Alimentacion_Nutricion
+{
+    public class Animales
+    {
+        public string Nombre { get; set; }
+        public int ID { get; set; }
+        public string Tipo { get; set; }
+        public double Peso { get; set; }
+    }
+}
