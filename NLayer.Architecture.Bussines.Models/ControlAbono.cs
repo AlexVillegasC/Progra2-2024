30845d68305cbadcc505d2d62bcba24fd4b25589
Alondra Flores <alondraespinoza897@gmail.com>
2024-05-08 00:42:37 -0600

    cositas

---
+++ NLayer.Architecture.Bussines.Models/ControlAbono.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace NLayer.Architecture.Bussines.GestionPlantaciones;
+
+public class ControlAbono
+{
+    public int FechaApli { get; set; }
+    public int Cantidad { get; set; }
+
+    public string Tipo { get; set; }
+
+
+}
