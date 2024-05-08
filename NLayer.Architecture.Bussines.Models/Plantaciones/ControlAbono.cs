52c54a4d7683ad8f942b7d8403b5f1c01a1455ec
Alondra Flores <alondraespinoza897@gmail.com>
2024-05-08 00:42:37 -0600

    cositas

--- NLayer.Architecture.Bussines.Models/Plantaciones/ControlAbono.cs
+++ NLayer.Architecture.Bussines.Models/Plantaciones/ControlAbono.cs
@@ -1,6 +1,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 ﻿namespace NLayer.Architecture.Bussines.GestionPlantaciones;
 
 public class ControlAbono
@@ -19,10 +20,14 @@ namespace NLayer.Architecture.Bussines.GestionPlantaciones;
 
 namespace NLayer.Architecture.Bussines.GestionPlantaciones;
 >>>>>>> c9e0984 (final de mi parte)
+=======
+﻿namespace NLayer.Architecture.Bussines.GestionPlantaciones;
+>>>>>>> 01278ca (cositas)
 
 public class ControlAbono
 {
     public int FechaApli { get; set; }
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 >>>>>>> c9e0984 (final de mi parte)
@@ -31,6 +36,9 @@ public class ControlAbono
 >>>>>>> 01278ca (cositas)
 =======
 >>>>>>> c9e0984 (final de mi parte)
+=======
+
+>>>>>>> 01278ca (cositas)
     public int Cantidad { get; set; }
 
     public string Tipo { get; set; }
