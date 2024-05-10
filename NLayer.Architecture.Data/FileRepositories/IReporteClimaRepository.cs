438735b2bb9afcab2dfdfd1493fb07d07102846d
alexvillegasc <alexvillegascarranza@gmail.com>
2024-05-09 20:53:14 -0600

    Advilleg - Post Temperatures

--- NLayer.Architecture.Data/FileRepositories/IReporteClimaRepository.cs
+++ NLayer.Architecture.Data/FileRepositories/IReporteClimaRepository.cs
@@ -4,9 +4,10 @@ namespace DataAccess.Layer.FileRepositories;
 
 public interface IReporteClimaRepository
 {
-    Task<Temperature> GetTemperatures();
+    Task<List<Temperature>> GetTemperatures();
+    Task AddTemperatures(Temperature temperature);
 
-    Task<Wind> GetWinds();
+    Task<List<Wind>> GetWinds();
 
-    Task<Moisture> GetMoisture();
+    Task<List<Moisture>> GetMoisture();
 }
