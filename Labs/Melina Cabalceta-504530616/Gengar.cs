a28cecb00406ccbf1b87831e9e9cc6d4960173b9
Melina1304 <melina.cabalceta.hernandez@est.una.ac.cr>
2024-05-14 18:47:47 -0500

    Batalla Pokemon

--- Labs/Melina Cabalceta-504530616/Gengar.cs
+++ Labs/Melina Cabalceta-504530616/Gengar.cs
@@ -4,15 +4,11 @@ using System.Linq;
 
 namespace Lab1.Melina_Cabalceta_504530616
 {
-    public class Gengar : Pokemon // Gengar hereda de Pokemon 
+    public class Gengar : Pokemon  
     {
         public Gengar(int ataque, int vida, string nombre) : base(ataque, vida, nombre)
-        {
-            this.Ataque = ataque;
-            this.Vida = vida;
-            this.Nombre = nombre;
-            
+        { 
         }
-
+        
     }
 }
