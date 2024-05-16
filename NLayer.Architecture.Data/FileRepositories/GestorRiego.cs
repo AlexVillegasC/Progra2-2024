515e750204d5806b1ee1c2b839aad802255a97c1
axelz <axel.castillo.zuniga@una.ac.cr>
2024-05-15 23:03:05 -0600

    capa de data

---
+++ NLayer.Architecture.Data/FileRepositories/GestorRiego.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Architecture.Data.FileRepositories
+{
+    public class GestorRiego:FileRepository, IGestorRiego
+    {
+        private string _cultivoVirtualPath = "cultivo.json";
+
+        private string _foldersPath {  get; set; }
+
+
+        public GestorRiego(IConfiguration configuration)
+        {
+            _foldersPath = $"{configuration["Folders:GestorRiego"]}";
+            _cultivoVirtualPath=_foldersPath+_cultivoVirtualPath;
+
+        }
+
+        public async Task<Cultivo> GetCultivo()
+        {
+            return await ReadJsonFileAsync < Cultivo>(_cultivoVirtualPath);
+        
+        
+        }
+    }
+}
---
+++ NLayer.Architecture.Data/FileRepositories/IGestorRiego.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Architecture.Data.FileRepositories
+{
+    public interface IGestorRiego
+    {
+        Task<Cultivo> GetCultivo();
+
+
+    }
+}
