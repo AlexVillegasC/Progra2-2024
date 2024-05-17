7da05393ade26fafc5eae0ead3fe262f7b016307
AriiH08 <ariana.hernandez.pena@est.una.ac.cr>
2024-05-16 23:17:20 -0600

    Creacion de la clase RegistroVeterinario

---
+++ NLayer.Architecture.Bussines.Models/RegistroGanado/RegistroVeterinario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Architecture.Bussines.Models.RegistroGanado
+{
+    public class RegistroVeterinario
+    {
+        public string Id { get; set; }
+        public int Date { get; set; }
+        public string Note { get; set; }
+        public bool State { get; set; }
+
+
+    }
+}
