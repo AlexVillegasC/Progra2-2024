13c2408ae8f3c3919158a31b3bbb78b83098824a
Jason Reyes Ocampos <130868181+Jason9783@users.noreply.github.com>
2024-05-17 15:44:41 -0600

    Capa de modelos

---
+++ NLayer.Architecture.Bussines.Models/Cultivo/Cultivo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Architecture.Bussines.Models.Cultivo;
+
+public class Cultivo
+{
+    public string Nombre { get; set; }
+    public string tipoCultivo { get; set; }
+    public DateTime fechaPlantacion { get; set; }
+    public string frecuenciaRiego { get; set; }
+
+}
---
+++ NLayer.Architecture.Bussines.Models/Cultivo/GestorRiego.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Architecture.Bussines.Models.Cultivo;
+
+public class GestorRiego
+{
+    public Temperatura _Temperatura { get; set; }
+    public HumedadSuelo _HumedadSuelo { get; set; }
+    public Cultivo _Cultivo { get; set; }
+
+}
