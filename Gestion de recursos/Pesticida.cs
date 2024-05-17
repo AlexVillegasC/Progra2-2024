0b5523d45888137ac78c41cc01ac866c27a200ea
Starlin06 <starlingquiros@gmail.com>
2024-05-17 14:27:26 -0600

    :)

---
+++ Gestion de recursos/Pesticida.cs
@@ -0,0 +1,5 @@
+public class Pesticida : Recurso
+{
+    public string tipo { get; set; }
+    public int cantidad { get; set; }
+}
---
+++ Gestion de recursos/Semilla.cs
@@ -0,0 +1,5 @@
+public class Semilla : Recurso
+{
+    public string tipo { get; set; }
+    public int cantidad { get; set; }
+}
