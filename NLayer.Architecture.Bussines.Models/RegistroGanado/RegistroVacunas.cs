ac06ea96b30dacfa566e087cb3e492add0ff0d77
Brandon-Corrales <brandon.nunez.corrales@est.una.ac.cr>
2024-05-16 22:45:05 -0600

    Creacion de Clase Registro_de_Vacunas y archivo .json

---
+++ NLayer.Architecture.Bussines.Models/RegistroGanado/RegistroVacunas.cs
@@ -0,0 +1,13 @@
+﻿
+namespace NLayer.Architecture.Bussines.Models.RegistroGanado
+{
+    public class Registro_de_Vacunas
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }        
+        public string Race { get; set; }
+        public bool Weigth { get; set; }
+
+    }
+}
