75143e523b7badd30733ef7d3eeb6f47e5274065
EstebanBonillaR <esteban.boniros@gmail.com>
2024-05-18 08:34:12 -0600

    #

---
+++ Gestion de recursos/Inventario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using System;
+using System.Collections.Generic;
+
+public class Inventario
+{
+    private List<Recurso> recursos = new List<Recurso>();
+
+    public void AgregarRecurso(Recurso recurso)
+    {
+        recursos.Add(recurso);
+    }
+
+    public void EliminarRecurso(int id)
+    {
+        recursos.RemoveAll(r => r.Id == id);
+    }
+
+    public void ActualizarCantidad(int id, int cantidad)
+    {
+        var recurso = recursos.Find(r => r.Id == id);
+        if (recurso != null)
+        {
+            recurso.Cantidad = cantidad;
+        }
+    }
+}
