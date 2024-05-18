705f043677c3e4e42dc2511af1561b35a144b803
FranderCarrillo <frander.carrillo.torres@est.una.ac.cr>
2024-05-17 18:50:24 -0600

    Actualizacion archivos json

--- NLayer.Architecture.Data/FileRepositories/IRegistro_de_Ganado.cs
+++
@@ -1,9 +0,0 @@
-﻿using NLayer.Architecture.Bussines.Models.RegistroGanado;
-namespace NLayer.Architecture.Data.FileRepositories;
-
-public interface IRegistro_de_Ganado
-{
-    Task<RegistroVeterinario> GetVeterinarios();
-    Task<List<RegistroGanado>> GetRegistroGanado();
-    Task<List<Registro_de_Vacunas>> GetRegistroVacunas();
-}
