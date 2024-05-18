81fc7bd9d36c404d06dde93053421a989d280f64
Jhonpy22 <jhonngranadosrojas@gmail.com>
2024-05-18 11:28:23 -0600

    Jhonpy

--- NLayer.Architecture.Bussines/Services/ReporteInventarioService.cs
+++ NLayer.Architecture.Bussines/Services/ReporteInventarioService.cs
@@ -44,6 +44,7 @@ namespace NLayer.Architecture.Bussines.Services;
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     
 <<<<<<< HEAD
 
@@ -121,6 +122,9 @@ namespace NLayer.Architecture.Bussines.Services;
         return miInventario;
     }
 >>>>>>> d564e34 (Herramientas)
+=======
+    
+>>>>>>> ba1bfd7 (Jhonpy)
 
     
 
