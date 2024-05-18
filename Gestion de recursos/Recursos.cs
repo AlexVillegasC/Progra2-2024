d21520c12f56e6ae2f77cf72d8aa398ac9e863b4
EstebanBonillaR <esteban.boniros@gmail.com>
2024-05-18 02:34:55 -0600

    #

---
+++ Gestion de recursos/Recursos.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Recursos
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public int Cantidad { get; set; }
+    public DateTime FechaCaducidad { get; set; }
+}
