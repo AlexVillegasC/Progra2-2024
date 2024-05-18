496ccc22e97636758522c20733f43b59c9bb7cc0
crissXcoder <cristhian.altamirano.montes@est.una.ac.cr>
2024-05-17 19:06:50 -0600

    Clase gestion cultivo y coeficiente cultivo

---
+++ NLayer.Architecture.Bussines.Models/GestionCultivo/GestionCultivo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Architecture.Bussines.Models.GestionCultivo
+{
+    public class GestionCultivo
+    {
+        public CoeficienteCultivo CoeficienteCultivo { get; set; }
+        public double Eto { get; set; }
+    }
+
+}
