49897f8b427f235149c99479687b844b4cfc866e
AriiH08 <ariana.hernandez.pena@est.una.ac.cr>
2024-05-17 19:01:26 -0600

    Repositorio e interfaz terminados.

---
+++ NLayer.Architecture.Data/FileRepositories/ReporteGanadoRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using NLayer.Architecture.Bussines.Models.RegistroGanado;
+using NLayer.Architecture.Data;
+
+namespace NLayer.Architecture.Data.FileRepositories
+{
+    public class ReporteRegistroGanadoRepository : FileRepository, IReporteRegistroGanadoRepository
+    {
+        private string _RegistroGanadoVirtualPath = "RegistroGanado.json";
+        private string _RegistroVacunasVirtualPath = "Registro_de_Vacunas.json";
+        private string _RegistroVeterinarioVirtualPath = "RegistroVeterinario.json";
+
+        private string FolderPath { get; set; }
+
+        public ReporteRegistroGanadoRepository(IConfiguration Configuration)
+        {
+            FolderPath = $"{Configuration["Folders::RegistroGanado"]}";
+            _RegistroGanadoVirtualPath = FolderPath + _RegistroGanadoVirtualPath;
+            _RegistroVacunasVirtualPath = FolderPath + _RegistroVacunasVirtualPath;
+            _RegistroVeterinarioVirtualPath = FolderPath + _RegistroVeterinarioVirtualPath;
+        }
+
+        public async Task<List<Registro_de_Vacunas>> GetRegistroVacunas()
+        {
+            return await ReadListJsonAsync<Registro_de_Vacunas>(_RegistroVacunasVirtualPath);
+        }
+
+        public async Task<List<RegistroGanado>> GetRegistroGanado()
+        {
+            return await ReadListJsonAsync<RegistroGanado>(_RegistroGanadoVirtualPath);
+        }
+
+        public async Task<List<RegistroVeterinario>> GetRegistroVeterinario()
+        {
+            return await ReadListJsonAsync<RegistroVeterinario>(_RegistroVeterinarioVirtualPath);
+        }
+    }
+}
