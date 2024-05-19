9d842eaa067398c3e183311e1c2fc8ecc0834847
crissXcoder <cristhian.altamirano.montes@est.una.ac.cr>
2024-05-18 23:24:07 -0600

    Cambios en las carpetas

---
+++ NLayer.Architecture.Bussines.Models/GestionCultivo/GestionCultivo.cs
@@ -0,0 +1,11 @@
+﻿namespace NLayer.Architecture.Bussines.GestionCultivo;
+
+public class GestionCultivo
+{
+    public string Nombre { get; set; } = "Gestion de Cultivos";
+
+    public CultivoDatos CultivoDatos { get; set; }
+    public Reservorio Reservorio { get; set; }
+    public CoeficienteCultivo CoeficienteCultivo { get; set; }
+    public double Eto { get; set; }
+}
