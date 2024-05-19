f335420d2fdd4e709d183a863c405431dca3f094
Car301 <carogonzalezjimenez@gmail.com>
2024-05-19 10:20:53 -0600

    Maria-Lluvia-Corto-Plazo

---
+++ PronosticoClimatico.Architecture.Bussines.Model/WeatherAverages.cs
@@ -0,0 +1,10 @@
+﻿// MonitoreoClimatico.Architecture.Bussines.Model/WeatherAverages.cs
+namespace MonitoreoClimatico.Architecture.Bussines.Model
+{
+    public class WeatherAverages
+    {
+        public WeatherModel Period1Average { get; set; }
+        public WeatherModel Period2Average { get; set; }
+        public WeatherModel Period3Average { get; set; }
+    }
+}
---
+++ PronosticoClimatico.Architecture.Bussines.Model/WeatherHistory.cs
@@ -0,0 +1,14 @@
+﻿// MonitoreoClimatico.Architecture.Bussines.Model/WeatherHistory.cs
+namespace MonitoreoClimatico.Architecture.Bussines.Model
+{
+    public class WeatherHistory
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public double Temperature { get; set; }
+        public string Description { get; set; }
+        public double Humidity { get; set; }
+        public double WindSpeed { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
