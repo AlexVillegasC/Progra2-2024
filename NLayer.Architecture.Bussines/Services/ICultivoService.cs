6093d8287b4c86e60c572171cdc3b640210a102b
crissXcoder <cristhian.altamirano.montes@est.una.ac.cr>
2024-05-18 23:17:00 -0600

    API GestionCultivo

---
+++ NLayer.Architecture.Bussines/Services/ICultivoService.cs
@@ -0,0 +1,8 @@
+﻿using NLayer.Architecture.Bussines.GestionCultivo;
+
+namespace NLayer.Architecture.Bussines.Services;
+
+public interface ICultivoService
+{
+    Task<GestionCultivo.GestionCultivo> GetCultivo();
+}
---
+++ NLayer.Architecture.Data/FileRepositories/ICultivoRepository.cs
@@ -0,0 +1,17 @@
+﻿using NLayer.Architecture.Bussines.GestionCultivo;
+
+namespace DataAccess.Layer.FileRepositories;
+
+
+public interface ICultivoRepository
+{
+    Task<GestionCultivo> GetGestionCultivo();
+
+    Task<CoeficienteCultivo> GetCoeficienteCultivo();
+
+    Task<CultivoDatos> GetCultivoDatos();
+
+    Task<Reservorio> GetReservorio();
+
+
+}
