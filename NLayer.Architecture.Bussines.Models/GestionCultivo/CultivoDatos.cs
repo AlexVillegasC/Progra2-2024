d79fcdb52dac25415d8b0528e3bb950fd78d16ff
crissXcoder <cristhian.altamirano.montes@est.una.ac.cr>
2024-05-19 00:16:01 -0600

    API GestionCultivo

---
+++ NLayer.Architecture.Bussines.Models/GestionCultivo/CultivoDatos.cs
@@ -0,0 +1,13 @@
+﻿namespace NLayer.Architecture.Bussines.GestionCultivo;
+
+public class CultivoDatos
+{
+    public string Nombre { get; set; }
+    public required string EtapaDesarrollo { get; set; }
+    public DateTime FechaSiembra { get; set; }
+    public DateTime FechaCosecha { get; set; }
+    public int CicloVegetativo { get; set; }
+    public double UsoConsuntivo { get; set; }
+    public double AreaCultivada { get; set; }
+    public double RendimientoPotencial { get; set; }
+}
