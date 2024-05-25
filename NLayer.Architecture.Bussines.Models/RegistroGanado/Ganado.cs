49eb19fb6dd09c67df9fd77990bd7fc3f2f9f4dc
FranderCarrillo <frander.carrillo.torres@est.una.ac.cr>
2024-05-24 19:37:44 -0600

    actualizacion proyyecto

--- NLayer.Architecture.Bussines.Models/RegistroGanado/Ganado.cs
+++ NLayer.Architecture.Bussines.Models/RegistroGanado/Ganado.cs
@@ -10,7 +10,7 @@ namespace NLayer.Architecture.Bussines.Models.RegistroGanado;
     public class Ganado
     {
         public string ID { get; set; }
-        public DateTime Date{ get; set; }
+        public DateOnly Date{ get; set; }
 
         public string Nombre { get; set; }
         public float Peso { get; set; }
--- NLayer.Architecture.Bussines.Models/RegistroGanado/RegistroVeterinario.cs
+++ NLayer.Architecture.Bussines.Models/RegistroGanado/RegistroVeterinario.cs
@@ -9,7 +9,7 @@ namespace NLayer.Architecture.Bussines.Models.RegistroGanado
     public class RegistroVeterinario
     {
         public string Id { get; set; }
-        public DateTime Date{ get; set; }
+        public DateOnly Date{ get; set; }
         public string Note { get; set; }
         public bool State { get; set; }
 
