dbbb615e5cc6411fa1b18e60f0dc40d00b54d287
alexvillegasc <alexvillegascarranza@gmail.com>
2024-05-24 22:29:11 -0600

    Clase 13

--- NLayer.Architecture.Bussines/Services/IReporteClimaService.cs
+++ NLayer.Architecture.Bussines/Services/IReporteClimaService.cs
@@ -7,4 +7,6 @@ public interface IReporteClimaService
 {
     Task<ReporteClima.ReporteClima> GetPronostico();
     Task AddTemperature(Temperature temperature);
+    Task<bool> UpdateTemperature(IEnumerable<Temperature> temperature);
+    Task<bool> DeleteTemperature();
 }
