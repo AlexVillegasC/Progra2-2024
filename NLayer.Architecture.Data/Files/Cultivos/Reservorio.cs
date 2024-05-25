fa71578b0bf49db9a5905b58d408ce046086125f
DannyOr94 <danny.abarca.alvarado@est.una.ac.cr>
2024-05-25 09:52:02 -0600

    Nuevos cambios del reservorio

---
+++ NLayer.Architecture.Data/Files/Cultivos/Reservorio.cs
@@ -0,0 +1,12 @@
+﻿namespace NLayer.Architecture.Bussines.GestionCultivo;
+
+public class Reservorio
+{
+    public double VolumeTotal { get; set; }
+    public double VolumenActual { get; set; }
+    public double Largo { get; set; }
+    public double Ancho { get; set; }
+    public double Profundidad { get; set; }
+    public double EvaporacionEstanque { get; set; }
+    public double AreaSuperficial { get; set; }
+}
