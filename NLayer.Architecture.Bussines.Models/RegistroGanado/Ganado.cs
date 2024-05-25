634786d88bc16d6484c6f1b788543bdde9b5c229
FranderCarrillo <frander.carrillo.torres@est.una.ac.cr>
2024-05-24 19:56:28 -0600

    prubeas

--- NLayer.Architecture.Bussines.Models/RegistroGanado/Ganado.cs
+++ NLayer.Architecture.Bussines.Models/RegistroGanado/Ganado.cs
@@ -11,6 +11,7 @@ namespace NLayer.Architecture.Bussines.Models.RegistroGanado;
     {
         public string ID { get; set; }
         public DateOnly Date{ get; set; }
+        
 
         public string Nombre { get; set; }
         public float Peso { get; set; }
