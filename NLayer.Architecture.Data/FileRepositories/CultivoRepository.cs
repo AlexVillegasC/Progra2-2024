7ff693aaf155b1153086e4c3acc9a895d27344f0
crissXcoder <cristhian.altamirano.montes@est.una.ac.cr>
2024-05-25 10:46:43 -0600

    se agrego el repositorio de cultivos

---
+++ NLayer.Architecture.Data/FileRepositories/CultivoRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using NLayer.Architecture.Bussines.GestionCultivo;
+using NLayer.Architecture.Data;
+using System.ComponentModel.Design;
+
+namespace DataAccess.Layer.FileRepositories;
+
+public class CultivoRepository : FileRepository, ICultivoRepository
+{
+    private string _coeficienteCultivoVirtualPath = "CoeficienteCultivo.json";
+    private string _cultivoDatosVirtualPath = "CultivoDatos.json";
+    private string _gestionCultivoVirtualPath = "GestionCultivo.json";
+    private string _reservorioVirtualPath = "reservorio.json";
+
+    private string FolderPath { get; set; }
+
+    public CultivoRepository(IConfiguration Configuration)
+    {
+        FolderPath = $"{Configuration["Folders:GestionCultivo"]}";
+        _gestionCultivoVirtualPath = FolderPath + _gestionCultivoVirtualPath;
+        _coeficienteCultivoVirtualPath = FolderPath + _coeficienteCultivoVirtualPath;
+        _cultivoDatosVirtualPath = FolderPath + _cultivoDatosVirtualPath;
+        _reservorioVirtualPath = FolderPath + _reservorioVirtualPath;
+        //
+    }
+
+    public async Task<GestionCultivo> GetGestionCultivo()
+    {
+        return await ReadJsonFileAsync<GestionCultivo>(_gestionCultivoVirtualPath);
+    }
+
+    public async Task<CoeficienteCultivo> GetCoeficienteCultivo()
+    {
+        return await ReadJsonFileAsync<CoeficienteCultivo>(_coeficienteCultivoVirtualPath);
+    }
+
+    public async Task<CultivoDatos> GetCultivoDatos()
+    {
+        return await ReadJsonFileAsync<CultivoDatos>(_cultivoDatosVirtualPath);
+    }
+
+    public async Task<Reservorio> GetReservorio()
+    {
+        return await ReadJsonFileAsync<Reservorio>(_reservorioVirtualPath);
+    }
+}
