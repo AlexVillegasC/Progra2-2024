7285a1195d86800bc588d474d315186b3228d2e9
Bairon <fallasbaltodanobairon@gmail.com>
2024-05-25 19:18:43 -0600

    Creacion de los servicios faltantes(Maquinaria) Bairon Fallas

--- NLayer.Architecture.API/Controllers/InventarioMaquinariaAgricola.cs
+++ NLayer.Architecture.API/Controllers/InventarioMaquinariaAgricola.cs
@@ -7,7 +7,7 @@ namespace NLayer.Architecture.API.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class InventarioMaquinariaAgricola :ControllerBase
+public class InventarioMaquinariaAgricola : ControllerBase
 {
     private readonly IReporteInventarioService _reporteInventarioService;
 
@@ -15,9 +15,34 @@ public class InventarioMaquinariaAgricola :ControllerBase
     {
         _reporteInventarioService = reporteInventarioService;
     }
+
     [HttpGet]
     public async Task<ReporteInventario> Get()
     {
         return await _reporteInventarioService.GetInventario();
     }
+
+    [HttpPost("AddMaquinaria", Name = "AddMaquinaria")]
+    public async Task AddMaquinaria(Maquinaria maqui)
+    {
+        await _reporteInventarioService.AddMaquinarias(maqui);
+    }
+
+    [HttpPut("UpdateMaquinaria", Name = "UpdateMaquinaria")]
+    public async Task<IActionResult> UpdateMaquinaria(IEnumerable<Maquinaria> updatedMaquinaria)
+    {
+        return await _reporteInventarioService.UpdateMaquinaria(updatedMaquinaria) ? Ok() : NotFound();
+    }
+
+    [HttpDelete("DeleteMaquinaria", Name = "DeleteMaquinaria")]
+    public async Task<IActionResult> DeleteMaquinaria()
+    {
+        return await _reporteInventarioService.DeleteMaquinaria() ? Ok() : NotFound();
+    }
 }
+
+/*
+Ok() : Devuelve un resultado de éxito con un código de estado HTTP 200 si la operación de actualización fue exitosa.
+NotFound(): Devuelve un resultado de "no encontrado" con un código de estado HTTP 404 si la operación de actualización 
+ no pudo llevarse a cabo(por ejemplo, si no se encontraron las temperaturas para actualizar).
+*/
--- NLayer.Architecture.Bussines/Services/ReporteInventarioService.cs
+++ NLayer.Architecture.Bussines/Services/ReporteInventarioService.cs
@@ -1,5 +1,8 @@
 ﻿
 using DataAccess.Layer.FileRepositories;
+using NLayer.Architecture.Bussines.ReporteClima;
+using NLayer.Architecture.Bussines.ReporteInventario;
+using System.Formats.Asn1;
 
 namespace NLayer.Architecture.Bussines.Services;
 
@@ -24,21 +27,32 @@ namespace NLayer.Architecture.Bussines.Services;
         // utilizando el repositorio proporcionado (_inventarioRepo).
         // Como este método es asincrónico, se usa 'await' para esperar su finalización.
         miInventario.Maquinaria = await _inventarioRepo.GetMaquinaria();
-        miInventario.Mantenimiento = await _inventarioRepo.GetMantenimiento();
-        miInventario.Herramientas = await _inventarioRepo.GetHerramientas();
+        //miInventario.Mantenimiento = await _inventarioRepo.GetMantenimiento();
+      //miInventario.Herramientas = await _inventarioRepo.GetHerramientas();
+
         return miInventario;
-       
-        
     }
 
-    
+    public async Task AddMaquinarias(Maquinaria maquinaria)
+    {
+        await _inventarioRepo.AddMaquinarias(maquinaria);
+    }
 
-    
+    public async Task<bool> UpdateMaquinaria(IEnumerable<Maquinaria> maquinaria)
+    {
+        return await _inventarioRepo.UpdateMaquinaria(maquinaria);
+    }
 
-    
+    public async Task<bool> DeleteMaquinaria()
+    {
+        return await _inventarioRepo.DeleteMaquinaria();
+    }
+
+
+
 }
-   
 
-
+//ELIMINAN "//" donde dice miInventario.Mantenimiento = await _inventarioRepo.GetMantenimiento();
+//Y crean lo que les corresponde que lo de los metodos
 
 
