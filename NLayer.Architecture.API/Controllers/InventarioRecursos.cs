ac2af209bf113c6b45dc30ea12ebf82fdcea4797
EstebanBonillaR <esteban.boniros@gmail.com>
2024-05-29 22:04:57 -0600

    #

---
+++ NLayer.Architecture.API/Controllers/InventarioRecursos.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using NLayer.Architecture.Bussines.ReporteInventario;
+using NLayer.Architecture.Bussines.Services;
+
+namespace NLayer.Architecture.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class InventarioRecursos : ControllerBase
+{
+    private readonly IReporteInventarioService _reporteInventarioService;
+
+    public InventarioRecursos(IReporteInventarioService reporteInventarioService)
+    {
+        _reporteInventarioService = reporteInventarioService;
+    }
+    [HttpGet]
+    public async Task<ReporteInventario> Get()
+    {
+        return await _reporteInventarioService.GetInventario();
+    }
+}
