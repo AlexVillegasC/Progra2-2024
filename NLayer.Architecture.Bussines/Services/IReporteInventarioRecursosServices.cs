b750ed53622039a2d254918c3034fce69fd0e0fe
Starlin06 <starlingquiros@gmail.com>
2024-05-29 22:13:34 -0600

    *2

---
+++ NLayer.Architecture.Bussines/Services/IReporteInventarioRecursosServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NLayer.Architecture.Bussines.Services;
+
+public interface IReporteInventarioRecursosServices
+{
+
+    Task<ReporteInventario.ReporteInventario> GetInventario();
+}
---
+++ NLayer.Architecture.Bussines/Services/ReporteInventarioRecursosServicecs.cs
@@ -0,0 +1,40 @@
+﻿
+using DataAccess.Layer.FileRepositories;
+
+namespace NLayer.Architecture.Bussines.Services;
+
+public class ReporteInventarioRecursosService : IReporteInventarioService
+{
+    //es un campo solo de lectura
+    private readonly IReporteInventarioRepository _inventarioRepo;
+
+    //constructor de la clase
+    public ReporteInventarioRecursosService(IReporteInventarioRepository inventarioRepo)
+    {
+        _inventarioRepo = inventarioRepo;
+    }
+
+    //implementacion del metodo
+    public async Task<ReporteInventario.ReporteInventario> GetInventario()
+    {
+        // Se crea una nueva instancia de Reporteinventario para almacenar el inventario
+        ReporteInventario.ReporteInventario miInventario = new ReporteInventario.ReporteInventario();
+
+        // Se obtienen las maquinaria relacionadas con el inventario agricola
+        // utilizando el repositorio proporcionado (_inventarioRepo).
+        // Como este método es asincrónico, se usa 'await' para esperar su finalización.
+        miInventario.Semillas = await _inventarioRepo.GetSemillas();
+        miInventario.Pesticidas = await _inventarioRepo.GetPesticidas();
+        miInventario.Fertilizantes = await _inventarioRepo.GetFertilizantes();
+        return miInventario;
+
+
+    }
+
+
+
+
+
+
+}
+
