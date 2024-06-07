644ace41e7bb66a62789fbde24cfc2cdac4834b5
FranderCarrillo <frander.carrillo.torres@est.una.ac.cr>
2024-06-07 15:50:35 -0600

    Se termina parte 2 del proyecto

--- NLayer.Architecture.API/Controllers/ReporteRegistroGanadoController.cs
+++ NLayer.Architecture.API/Controllers/ReporteRegistroGanadoController.cs
@@ -2,9 +2,9 @@ using Microsoft.AspNetCore.Mvc;
 using NLayer.Architecture.Bussines.Services;
 using NLayer.Architecture.Bussines.Models.RegistroGanado;
 using NLayer.Architecture.Bussines.RegistroGanado;
-
-
+using NLayer.Architecture.Bussines;
 namespace NLayer.Architecture.API.Controllers;
+using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
 [Route("[controller]")]
@@ -25,6 +25,36 @@ public class ReporteRegistroGanadoController : ControllerBase
     {
         return await _reporteGanadoService.GetRegistroGanado();
     }
+
+     [HttpPost("AddGanado", Name = "AddGanado")]
+        public async Task<IActionResult> AddGanado([FromBody] Ganado ganado)
+        {
+            if (ganado == null)
+            {
+                return BadRequest();
+            }
+
+            await _reporteGanadoService.AddGanado(ganado);
+            return Ok();
+        }
+        [HttpPut("UpdateGanado", Name = "UpdateGanado")]
+        public async Task<IActionResult> UpdateGanado([FromBody] IEnumerable<Ganado> updatedGanado)
+        {
+            if (updatedGanado == null)
+            {
+                return BadRequest();
+            }
+
+            bool result = await _reporteGanadoService.UpdateGanado(updatedGanado);
+            return result ? Ok() : NotFound();
+        }
+
+        [HttpDelete("DeleteGanado", Name = "DeleteGanado")]
+        public async Task<IActionResult> DeleteTemperature()
+        {
+        return await _reporteGanadoService.DeleteGanado() ? Ok() : NotFound();    
+        }
+
 }
 
 
--- NLayer.Architecture.Bussines/Services/ReporteRegistroGanadoServiceFrander.cs
+++ NLayer.Architecture.Bussines/Services/ReporteRegistroGanadoServiceFrander.cs
@@ -1,8 +1,10 @@
 using DataAccess.Layer.FileRepositories;
 using Newtonsoft.Json.Linq;
+using NLayer.Architecture.Bussines.Models.RegistroGanado;
 using NLayer.Architecture.Bussines.RegistroGanado;
 using NLayer.Architecture.Data.FileRepositories;
 using System.Collections.Generic;
+using System.Reflection.Metadata;
 
 namespace NLayer.Architecture.Bussines.Services;
 
@@ -23,6 +25,20 @@ public class ReporteRegistroGanadoService : IReporteRegistroGanadoService
         miRegistro.ListaRegistroVet = await _ReporteGanadoRepo.GetRegistroVeterinario();
 
         return miRegistro;
+    }
+
+    public async Task AddGanado (Ganado ganado)
+    {
+        await _ReporteGanadoRepo.AddGanado(ganado);
+    }
+
+    public async Task<bool> UpdateGanado (IEnumerable<Ganado> ganado)
+    {
+        return await _ReporteGanadoRepo.UpdateGanado(ganado);
+    }
+
+    public async Task <bool> DeleteGanado (){
+        return await _ReporteGanadoRepo.DeleteGanado();
     }
 
 }
