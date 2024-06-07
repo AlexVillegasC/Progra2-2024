7588d24a4a74b83f533059e8f842a5a590997ddc
FranderCarrillo <frander.carrillo.torres@est.una.ac.cr>
2024-06-06 20:33:54 -0600

    Se termina el lab5

---
+++ Lab5/frander-504440503/agregacion/profesor.cs
@@ -0,0 +1,13 @@
+namespace Lab5.frander_504440503.agregacion;
+
+public class Profesor
+{
+    public string Nombre { get; set; }
+    public string Materia { get; set; }
+
+    public Profesor(string nombre, string materia)
+    {
+        Nombre = nombre;
+        Materia = materia;
+    }
+}
---
+++ Lab5/frander-504440503/composicion/Pulmon.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab5.frander_504440503.composicion;
+public class Pulmon
+{
+    public string FuncionRespiratoria { get; set; }
+
+    public Pulmon()
+    {
+        FuncionRespiratoria = "Respiración";
+    }
+}
