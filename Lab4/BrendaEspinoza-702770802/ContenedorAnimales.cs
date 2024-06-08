82c0fd0ae7fdc74f4c7c28dc5fd04c1b12da2a27
Brenda <brenda.espinoza.matarrita@est.una.ac.cr>
2024-06-07 22:33:20 -0600

    Cambios al lab4

--- Lab4/BrendaEspinoza-702770802/ContenedorAnimales.cs
+++ Lab4/BrendaEspinoza-702770802/ContenedorAnimales.cs
@@ -2,15 +2,15 @@ namespace Lab4.BrendaEspinoza_702770802.Animales;
 
 public class ContenedorAnimalesV<T>
 {
-    private List<T> elementos = new List<T>(); // Lista para almacenar elementos de cualquier tipo
+    private List<T> elementos = new List<T>(); 
 
-    // Método para agregar elementos al contenedor
+   
     public void Agregar(T elemento)
     {
         elementos.Add(elemento);
     }
 
-    // Método para mostrar los elementos del contenedor
+    
     public void MostrarElementos()
     {
         Console.WriteLine("Elementos en el contenedor:");
