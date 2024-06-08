fc55113141e73a0bd876439d9ac1b9654c23baa0
Brenda <brenda.espinoza.matarrita@est.una.ac.cr>
2024-06-08 14:27:50 -0600

    Nuevos cambios en el cultivo

--- NLayer.Architecture.Data/FileRepositories/CultivoRepository.cs
+++ NLayer.Architecture.Data/FileRepositories/CultivoRepository.cs
@@ -24,23 +24,156 @@ public class CultivoRepository : FileRepository, ICultivoRepository
         //
     }
 
-    public async Task<GestionCultivo> GetGestionCultivo()
+    public async Task<List<GestionCultivo>> GetGestionCultivo()
     {
-        return await ReadJsonFileAsync<GestionCultivo>(_gestionCultivoVirtualPath);
+        return await ReadJsonFileAsync<List<GestionCultivo>>(_gestionCultivoVirtualPath);
     }
 
-    public async Task<CoeficienteCultivo> GetCoeficienteCultivo()
+    public async Task<List<CoeficienteCultivo>> GetCoeficienteCultivo()
     {
-        return await ReadJsonFileAsync<CoeficienteCultivo>(_coeficienteCultivoVirtualPath);
+        return await ReadJsonFileAsync<List<CoeficienteCultivo>>(_coeficienteCultivoVirtualPath);
     }
 
-    public async Task<CultivoDatos> GetCultivoDatos()
+    public async Task<List<CultivoDatos>> GetCultivoDatos()
     {
-        return await ReadJsonFileAsync<CultivoDatos>(_cultivoDatosVirtualPath);
+        return await ReadJsonFileAsync<List<CultivoDatos>>(_cultivoDatosVirtualPath);
     }
 
-    public async Task<Reservorio> GetReservorio()
+    public async Task<List<Reservorio>> GetReservorio()
     {
-        return await ReadJsonFileAsync<Reservorio>(_reservorioVirtualPath);
+        return await ReadJsonFileAsync<List<Reservorio>>(_reservorioVirtualPath);
+    }
+
+    public async Task AddGestionCultivo(GestionCultivo gestionCultivo)
+    {
+        List<GestionCultivo> elementos = await GetGestionCultivo();
+        elementos.Add(gestionCultivo);
+        await WriteJsonFileAsync(_gestionCultivoVirtualPath, elementos);
+    }
+    public async Task<bool> UpdateGestionCultivo(IEnumerable<GestionCultivo> gestionCultivo)
+    {
+        List<GestionCultivo> elementos = gestionCultivo.ToList();
+
+        try
+        {
+            await WriteJsonFileAsync(_gestionCultivoVirtualPath, elementos);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public async Task<bool> DeleteGestionCultivo()
+    {
+        try
+        {
+            await WriteJsonFileAsync(_gestionCultivoVirtualPath, new List<GestionCultivo>());
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public async Task AddCultivoDatos(CultivoDatos cultivoDatos)
+    {
+        List<CultivoDatos> elementos = await GetCultivoDatos();
+        elementos.Add(cultivoDatos);
+        await WriteJsonFileAsync(_cultivoDatosVirtualPath, elementos);
+    }
+    public async Task<bool> UpdateCultivoDatos(IEnumerable<CultivoDatos> cultivoDatos)
+    {
+        List<CultivoDatos> elementos = cultivoDatos.ToList();
+
+        try
+        {
+            await WriteJsonFileAsync(_cultivoDatosVirtualPath, elementos);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    } 
+    public async Task<bool> DeleteCultivo()
+    {
+        try
+        {
+            await WriteJsonFileAsync(_cultivoDatosVirtualPath, new List<CultivoDatos>());
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public async Task AddReservorio(Reservorio reservorio)
+    {
+        List<Reservorio> elementos = await GetReservorio();
+        elementos.Add(reservorio);
+        await WriteJsonFileAsync(_reservorioVirtualPath, elementos);
+    }
+    public async Task<bool> UpdateReservorio(IEnumerable<Reservorio> reservorio)
+    {
+        List<Reservorio> elementos = reservorio.ToList();
+
+        try
+        {
+            await WriteJsonFileAsync(_reservorioVirtualPath, elementos);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+    public async Task<bool> DeleteReservorio()
+    {
+        try
+        {
+            await WriteJsonFileAsync(_reservorioVirtualPath, new List<Reservorio>());
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public async Task AddCoeficienteCultivo(CoeficienteCultivo coeficienteCultivo)
+    {
+        List<CoeficienteCultivo> elementos = await GetCoeficienteCultivo();
+        elementos.Add(coeficienteCultivo);
+        await WriteJsonFileAsync(_coeficienteCultivoVirtualPath, elementos);
+    }
+    public async Task<bool> UpdateCoeficienteCultivo(IEnumerable<CoeficienteCultivo> coeficienteCultivo)
+    {
+        List<CoeficienteCultivo> elementos = coeficienteCultivo.ToList();
+
+        try
+        {
+            await WriteJsonFileAsync(_coeficienteCultivoVirtualPath, elementos);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+    public async Task<bool> DeleteCoeficienteCultivo()
+    {
+        try
+        {
+            await WriteJsonFileAsync(_coeficienteCultivoVirtualPath, new List<CoeficienteCultivo>());
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
     }
 }
--- NLayer.Architecture.Data/FileRepositories/ICultivoRepository.cs
+++ NLayer.Architecture.Data/FileRepositories/ICultivoRepository.cs
@@ -5,13 +5,30 @@ namespace DataAccess.Layer.FileRepositories;
 
 public interface ICultivoRepository
 {
-    Task<GestionCultivo> GetGestionCultivo();
+    Task<List<GestionCultivo>> GetGestionCultivo();
 
-    Task<CoeficienteCultivo> GetCoeficienteCultivo();
+    Task<List<CoeficienteCultivo>> GetCoeficienteCultivo();
 
-    Task<CultivoDatos> GetCultivoDatos();
+    Task<List<CultivoDatos>> GetCultivoDatos();
 
-    Task<Reservorio> GetReservorio();
+    Task<List<Reservorio>> GetReservorio();
 
+    Task AddGestionCultivo(GestionCultivo gestionCultivo);
 
+    Task<bool> UpdateGestionCultivo(IEnumerable<GestionCultivo> gestionCultivo);
+
+    Task <bool> DeleteGestionCultivo();
+
+    Task AddCultivoDatos(CultivoDatos cultivoDatos);
+    Task<bool> UpdateCultivoDatos(IEnumerable<CultivoDatos> cultivoDatos);
+
+    Task<bool> DeleteCultivo();
+
+    Task AddReservorio(Reservorio reservorio);
+    Task<bool> UpdateReservorio(IEnumerable<Reservorio> reservorio);
+    Task<bool> DeleteReservorio();
+
+    Task AddCoeficienteCultivo(CoeficienteCultivo coeficienteCultivo);
+    Task<bool> UpdateCoeficienteCultivo(IEnumerable<CoeficienteCultivo> coeficienteCultivo);
+    Task<bool> DeleteCoeficienteCultivo();
 }
