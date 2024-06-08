c2f2ce2c1039afb5abd8749ad2abd6256da1130e
DannyOr94 <danny.abarca.alvarado@est.una.ac.cr>
2024-06-08 14:39:08 -0600

    Entraga de la segunda parte del proyecto final, donde se agregan cambios en el service, interfaz y Reservorio.json.

--- NLayer.Architecture.Bussines/Services/CultivoService.cs
+++ NLayer.Architecture.Bussines/Services/CultivoService.cs
@@ -1,4 +1,5 @@
 using DataAccess.Layer.FileRepositories;
+using NLayer.Architecture.Bussines.GestionCultivo;
 
 namespace NLayer.Architecture.Bussines.Services;
 
@@ -22,4 +23,57 @@ public class CultivoService : ICultivoService
 
         return miCultivo;
     }
+
+    public async Task AddCultivos(GestionCultivo.GestionCultivo cultivo)
+    {
+        await _cultivoRepository.AddGestionCultivo(cultivo);
+    }
+    public async Task<bool> UpdateCultivo(IEnumerable<GestionCultivo.GestionCultivo> cultivo)
+    {
+        return await _cultivoRepository.UpdateGestionCultivo(cultivo);
+    }
+    public async Task DeleteCultivo()
+    {
+        await _cultivoRepository.DeleteGestionCultivo();
+    }
+
+    public async Task AddCultivoDatos(CultivoDatos cultivoDatos)
+    {
+        await _cultivoRepository.AddCultivoDatos(cultivoDatos);
+    }
+    public async Task<bool> UpdateCultivoDatos(IEnumerable<CultivoDatos> cultivoDatos)
+    {
+        return await _cultivoRepository.UpdateCultivoDatos(cultivoDatos);
+    }
+    public async Task DeleteCultivoDatos()
+    {
+        await _cultivoRepository.DeleteCultivo();
+    }
+
+    public async Task AddReservorio(Reservorio reservorio)
+    {
+        await _cultivoRepository.AddReservorio(reservorio);
+    }
+    public async Task<bool> UpdateReservorio(IEnumerable<Reservorio> reservorio)
+    {
+        return await _cultivoRepository.UpdateReservorio(reservorio);
+    }
+    public async Task DeleteReservorio()
+    {
+        await _cultivoRepository.DeleteReservorio();
+    }
+
+    public async Task AddCoeficienteCultivo(CoeficienteCultivo coeficienteCultivo)
+    {
+        await _cultivoRepository.AddCoeficienteCultivo(coeficienteCultivo);
+    }
+    public async Task<bool> UpdateCoeficienteCultivo(IEnumerable<CoeficienteCultivo> coeficienteCultivo)
+    {
+        return await _cultivoRepository.UpdateCoeficienteCultivo(coeficienteCultivo);
+    }
+    public async Task DeleteCoeficienteCultivo()
+    {
+        await _cultivoRepository.DeleteCoeficienteCultivo();
+    }
+
 }
