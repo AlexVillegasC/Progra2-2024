386d559370b11e43768724801f75af23306241ec
DannyOr94 <danny.abarca.alvarado@est.una.ac.cr>
2024-06-14 16:46:39 -0600

    Lab 5 de agregacion y composicion usando tema de lego

---
+++ Lab5/DannyAlvarado-504000385/Composicion/Manual.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab5.Danny_Alvarado_504000385.Composicion;
+
+public class Manual
+    {
+        public string Idioma { get; set; }
+        public int Paginas { get; set; }
+
+        public Manual(string idioma, int paginas)
+        {
+            Idioma = idioma;
+            Paginas = paginas;
+        }
+
+        public override string ToString()
+        {
+            return $"Idioma: {Idioma}, Páginas: {Paginas}";
+        }
+    }
+
+
---
+++ Lab5/DannyAlvarado-504000385/Composicion/Pieza.cs
@@ -0,0 +1,19 @@
+﻿namespace Lab5.Danny_Alvarado_504000385.Composicion;
+
+    public class Pieza
+    {
+        public string Tipo { get; set; }
+        public string Color { get; set; }
+
+        public Pieza(string tipo, string color)
+        {
+            Tipo = tipo;
+            Color = color;
+        }
+
+        public override string ToString()
+        {
+            return $"Tipo: {Tipo}, Color: {Color}";
+        }
+    }
+
