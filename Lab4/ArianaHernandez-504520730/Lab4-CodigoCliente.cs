e8e1e697e3399afbced37fe691e1aeaf27f6463d
AriiH08 <ariana.hernandez.pena@est.una.ac.cr>
2024-06-15 15:57:22 -0600

    Lab 4

---
+++ Lab4/ArianaHernandez-504520730/Lab4-CodigoCliente.cs
@@ -0,0 +1,12 @@
+﻿
+//using Lab4.ArianaHernandez_504520730.Frutas;
+//using Lab4.ArianaHernandez_504520730;
+
+//ContenedorFrutas<Frutas> miContenedorFrutas = new Lab4.ArianaHernandez_504520730.ContenedorFrutas<Frutas>();
+//Frutas manzana = new Frutas("Manzana");
+//Frutas banana = new Frutas("Banana");
+//Frutas naranja = new Frutas("Naranja");
+//miContenedorFrutas.Agregar(manzana);
+//miContenedorFrutas.Agregar(banana);
+//miContenedorFrutas.Agregar(naranja);
+//miContenedorFrutas.MostrarElementos();
