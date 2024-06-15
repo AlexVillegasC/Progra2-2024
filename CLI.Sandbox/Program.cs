9ae1ca9b5934ad13c612b3b501059a028de3305c
AriiH08 <ariana.hernandez.pena@est.una.ac.cr>
2024-06-15 15:07:27 -0600

    Lab 1

--- CLI.Sandbox/Program.cs
+++ CLI.Sandbox/Program.cs
@@ -1,25 +1,8 @@
-﻿//using System;
+﻿
+
+
 
 
 //// Usar solo para pruebas!
 Console.WriteLine("Hello World!");
 
-//namespace Lab2.ArianaHernandez_504520730
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Pokemon pikachu = new Pikachu(55, 35, "Pikachu", 5);
-//            Pokemon charizard = new Charizard(84, 78, "Charizard", 36);
-
-//            pikachu.Presentacion();
-//            pikachu.MovimientoEspecial();
-
-//            Console.WriteLine();
-
-//            charizard.Presentacion();
-//            charizard.MovimientoEspecial();
-//        }
-//    }
-//}
