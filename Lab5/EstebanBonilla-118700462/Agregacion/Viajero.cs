603adf2447fd54e667d0cbd25fe67242dc352f92
EstebanBonillaR <esteban.boniros@gmail.com>
2024-06-15 14:51:27 -0600

    #

---
+++ Lab5/EstebanBonilla-118700462/Agregacion/Viajero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5.EstebanBonilla_118700462.Agregacion;
+public class Viajero
+{
+    public int Identificacion { get; set; }
+
+    public string PrimerNombre { get; set; }
+
+    public string PrimerApellido { get; set; }
+
+    public Viajero(int identificacion, string primerNombre, string primerApellido)
+    {
+        this.Identificacion = identificacion;
+        this.PrimerNombre = primerNombre;
+        this.PrimerApellido = primerApellido;
+    }
+}
---
+++ Lab5/EstebanBonilla-118700462/Composicion/Pistones.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5.EstebanBonilla_118700462.Composicion;
+public class Pistones
+{
+    public string FuncionCompresion { get; set; }
+
+    public Pistones()
+    {
+        FuncionCompresion = "Compresión de la mezcla aire-combustible";
+    }
+}
